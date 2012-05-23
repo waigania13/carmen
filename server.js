54f5d70501ff7b36213d074cdca3fc85defe5a99
Young Hahn <young@developmentseed.org>
2012-05-23 01:37:51 -0400

    Don't need bones, just use express.

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-process.argv[0] = 'node';
-require('bones').load(__dirname);
-!module.parent && require('bones').start();
+var express = require('express');
+var server = express.createServer();
+var carmen = new (require('./index.js'))();
+
+server.get('/api/:query', function(req, res, next) {
+    if (!req.param('query')) return res.send(404);
+    carmen.geocode(req.param('query'), function(err, data) {
+        if (err) return next(err);
+        res.send(data);
+    });
+});
+
+server.use(express['static'](__dirname + '/static'));
+
+server.listen(3000);
