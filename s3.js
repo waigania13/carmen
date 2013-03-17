0406b42d8d0b75132c04a8fea87bfc714c267548
Young Hahn <young@developmentseed.org>
2013-03-17 15:34:43 -0400

    Sketch of tilelive-s3 as carmen backend.

---
+++ s3.js
@@ -0,0 +1,206 @@
+var _ = require('underscore');
+var S3 = require('tilelive-s3');
+var url = require('url');
+var path = require('path');
+var zlib = require('zlib');
+
+module.exports = S3;
+
+// From mapbox-streets carto_table_updates.sql.
+// @TODO this should be defined per-source, not hardcoded into the API.
+var abbr = [
+    'Intl',
+    'Mt',
+    'Natl',
+    'St',
+    'E',
+    'N',
+    'NE',
+    'NW',
+    'S',
+    'SE',
+    'SW',
+    'W',
+    'Ave',
+    'Blvd',
+    'Cir',
+    'Ct',
+    'Cr',
+    'Dr',
+    'Exwy',
+    'Fwy',
+    'Hwy',
+    'Ln',
+    'Pk',
+    'Pkwy',
+    'Pt',
+    'Pl',
+    'Rd',
+    'Sq',
+    'St',
+    'Ter',
+    'str',
+
+    // Others that should not standalone...
+    'of',
+    'the',
+    'saint',
+    'republic',
+    'north',
+    'south',
+    'east',
+    'west',
+    'island',
+    'and',
+    'new'
+].map(function(s) { return s.toLowerCase() });
+
+S3.terms = function(doc) {
+    var parts = doc.split(' ')
+        .map(function(w) { return w.replace(/[^A-Za-z]/g, '').toLowerCase(); })
+        .filter(function(w) { return w.length });
+    return []
+        .concat(parts.filter(function(w) { return abbr.indexOf(w) === -1 }))
+        .concat(parts.length > 1 ? parts.join('_') : []);
+};
+
+// Implements carmen#search method.
+S3.prototype.search = function(query, id, callback) {
+    if (!this.data) return callback(new Error('Tilesource not loaded'));
+
+    // Parse carmen URL.
+    try { var uri = url.parse(this.data._carmen); }
+    catch (err) { return callback(new Error('Carmen not supported')); }
+
+    new S3.get({
+        uri: url.format({
+            hostname:uri.hostname,
+            protocol:uri.protocol,
+            query:{prefix:path.join(uri.pathname, 'term/' + S3.terms(query).pop()).substr(1)}
+        }),
+        headers: {Connection:'Keep-Alive'},
+        agent: S3.agent
+    }).asBuffer(function(err, buffer) {
+        if (err) return callback(err);
+        var xml = buffer.toString('utf8');
+        var parsed = xml.match(new RegExp('[^>]+(?=<\\/Key>)', 'g')) || [];
+        var docs = _(parsed).chain()
+            .reduce(function(memo, obj) {
+                var key = obj.split('/').pop().split('.');
+                memo[key[1]] = memo[key[1]] || {};
+                memo[key[1]].id = key[1];
+                memo[key[1]].text = key[0];
+                memo[key[1]].zxy = (memo[key[1]].zxy || [])
+                    .concat(key.slice(2).map(function(v) { return v.replace(/,/g,'/') }));
+                return memo;
+            }, {})
+            .toArray()
+            .value();
+        return callback(null, docs);
+    }.bind(this));
+};
+
+// Implements carmen#feature method.
+S3.prototype.feature = function(id, callback, raw) {
+    if (!this.data) return callback(new Error('Tilesource not loaded'));
+
+    // Parse carmen URL.
+    try { var uri = url.parse(this.data._carmen); }
+    catch (err) { return callback(new Error('Carmen not supported')); }
+
+    uri.pathname = path.join(uri.pathname, 'data/' + id + '.json');
+    new S3.get({
+        uri: url.format(uri),
+        headers: {Connection:'Keep-Alive'},
+        agent: S3.agent
+    }).asBuffer(function(err, buffer) {
+        if (err) return callback(err);
+        try { var data = JSON.parse(buffer.toString('utf8')); }
+        catch (err) { return callback(err); }
+        if (!raw) delete data._terms;
+        return callback(null, data);
+    });
+};
+
+// Implements carmen#index method.
+S3.prototype.index = function(id, text, doc, zxy, callback) {
+    if (!this.data) return callback(new Error('Tilesource not loaded'));
+    if (!this.client) return callback(new Error('Tilesource not writing'));
+
+    // @TODO get existing document and purge its terms.
+
+    // Parse carmen URL.
+    try { var uri = url.parse(this.data._carmen); }
+    catch (err) { return callback(new Error('Carmen not supported')); }
+
+    // Add each search term shard.
+    var terms = [];
+    var puts = [];
+    S3.terms(text).forEach(function(shard) {
+        var coords = zxy.map(function(zxy) { return zxy.replace(/\//g,',') });
+        var prefix = path.join(uri.pathname, 'term/' + shard + '.' + id + '.');
+        while (coords.length) {
+            var chunk = [];
+            do {
+                var next = prefix + chunk.join('.') + (coords.length ? '.' + coords[0] : '');
+            } while (
+                next.length < 1024 &&
+                coords.length &&
+                chunk.push(coords.shift())
+            );
+            var key = prefix + chunk.join('.');
+            terms.push(key);
+            puts.push({ key:key, data:'' });
+        }
+    });
+
+    // Add actual doc to queue.
+    puts.push({
+        key:path.join(uri.pathname, 'data/' + id + '.json'),
+        data:JSON.stringify(_({_terms:terms}).defaults(doc))
+     });
+
+    var put = function(err) {
+        if (!puts.length) return callback();
+        if (err) {
+            console.error(err);
+            return setTimeout(put, 500);
+        }
+
+        var obj = puts[0];
+        var req = this.client.put(obj.key, {
+            'x-amz-acl': 'public-read',
+            'Connection': 'keep-alive',
+            'Content-Length': obj.data ? Buffer.byteLength(obj.data, 'utf8') : 0,
+            'Content-Type': 'application/json'
+        });
+        req.on('close', put);
+        req.on('error', put);
+        req.setTimeout(60e3, function() {
+            req.abort();
+            var err = new Error('ESOCKETTIMEDOUT');
+            err.code = 'ESOCKETTIMEDOUT';
+            req.emit('error', err);
+        });
+        req.on('response', function(res) {
+            res.on('error', put);
+            if (res.statusCode === 200) {
+                puts.shift();
+                return put();
+            } else {
+                console.warn(res);
+                return put(new Error('S3 put failed: ' + res.statusCode));
+            }
+        });
+        req.end(obj.data);
+    }.bind(this);
+
+    put();
+};
+
+// Implements carmen#indexable method.
+S3.prototype.indexable = function(callback) {
+    // @TODO
+    return callback();
+};
+
---
+++ scripts/tos3.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var argv = process.argv;
+var MBTiles = require('../mbtiles');
+var mbtiles = argv[2];
+var S3 = require('../s3');
+var s3 = argv[3];
+
+if (!mbtiles) {
+    console.warn('Usage: tos3.js <from.mbtiles> <to.s3>');
+    process.exit(1);
+}
+if (!fs.existsSync(mbtiles)) {
+    console.warn('File %s does not exist.', mbtiles);
+    process.exit(1);
+}
+if (!fs.existsSync(s3)) {
+    console.warn('File %s does not exist.', s3);
+    process.exit(1);
+}
+
+new MBTiles(mbtiles, function(err, from) {
+    if (err) throw err;
+    new S3(s3, function(err, s3) {
+        if (err) throw err;
+        s3.startWriting(function(err) {
+            if (err) throw err;
+            from.indexable(function(err, docs) {
+                if (err) throw err;
+                var write = function() {
+                    if (!docs.length) return s3.stopWriting(function(err) {
+                        if (err) throw err;
+                        console.log('Done.');
+                    });
+                    var doc = docs.shift();
+                    s3.index(doc.id, doc.text, doc.doc, doc.zxy, function(err) {
+                        if (err) throw err;
+                        write();
+                    });
+                };
+                write();
+            });
+        });
+    });
+});
+
