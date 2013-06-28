2052e91b38fce1357a845f576ac3bfe062c5da70
Young Hahn <young@developmentseed.org>
2013-06-28 11:48:05 -0400

    Use dev indexes.

--- scripts/install-dbs.sh
+++ scripts/install-dbs.sh
@@ -7,18 +7,12 @@ if [ ! -d "$TILE_DIR" ]; then
     mkdir "$TILE_DIR"
 fi
 
-TILES="osm-places ne-countries ne-provinces tiger-zipcodes"
+TILES="mb-places ne-countries ne-provinces tiger-zipcodes"
 
 for NAME in $TILES; do
   if [ ! -f "$TILE_DIR/$NAME.mbtiles" ]; then
     echo "Downloading $NAME..."
-    curl -s -o "$TILE_DIR/$NAME.mbtiles" "http://mapbox-carmen.s3.amazonaws.com/carmen/$NAME.mbtiles"
+    curl -s -o "$TILE_DIR/$NAME.mbtiles" "http://mapbox-carmen.s3.amazonaws.com/dev/$NAME.mbtiles"
   fi
 done
 
-for NAME in $TILES; do
-  if [ -z "$(sqlite3 "$TILE_DIR/$NAME.mbtiles" "select 1 from sqlite_master where name = 'carmen' and type = 'table'")" ]; then
-    echo "Indexing $NAME..."
-    $DIR/carmen-index.js "$TILE_DIR/$NAME.mbtiles"
-  fi
-done
