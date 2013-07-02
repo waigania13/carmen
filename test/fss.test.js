292ee11eb6dafe9347d66737e24f73249228b4fb
artemp <artem@mapnik.org>
2013-07-02 12:54:47 -0400

    update test

--- test/fss.test.js
+++ test/fss.test.js
@@ -4,8 +4,9 @@ var assert = require('assert');
 describe('fss', function() {
     it('should work', function(done) {
         var engine = new fss.Engine();
-        engine.add({file:'foo.txt',distance:10});
-        console.log(engine.query('foo'));
+        engine.add({file:'/Users/artem/Projects/fss/data/places.txt',distance:2});
+        var results = engine.search({query:"Oxfordd Street", distance:2, num_results:3});
+        console.log(results);
         done();
     });
 });
