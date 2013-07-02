dd6ee2fd813be99737bf482c8cab34ea1c9bed00
artemp <artem@mapnik.org>
2013-07-02 14:14:42 -0400

    return 10 terms by default

--- fss_server.js
+++ fss_server.js
@@ -6,14 +6,14 @@ var express = require('express');
 var server = express.createServer();
 var fss_engine = new fss.Engine();
 
-fss_engine.add({file:'/Users/artem/Projects/fss/data/places.txt',distance:2});
+fss_engine.add({file:'/Users/artem/Projects/fss/data/geonames-fixed',distance:2});
 
 server.get('/fss/:query', function(req, res, next)
 {
     if (!req.param('query')) return res.send(404);
     console.log(req.param('query'));
     console.time('fss ' + req.param('query'));
-    var result = fss_engine.search({query:req.param('query'), distance:2, num_results:3});
+    var result = fss_engine.search({query:req.param('query'), distance:2, num_results:10});
     res.send(result);
 });
 
