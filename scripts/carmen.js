f80d386a7e8a4025d99fce198ad54435af2e5bf8
root <root@geocoder.mapbox.com>
2013-08-01 07:38:36 -0400

    Show stats at the end.

--- scripts/carmen.js
+++ scripts/carmen.js
@@ -16,10 +16,16 @@ carmen.geocode(process.argv[2], function(err, data) {
     if (err) throw err;
     carmen.geocode(process.argv[2], function(err, data) {
         if (err) throw err;
-        console.warn(data.query);
-        console.warn(data.stats);
-        data.results.forEach(function(r) {
+        var results = data.results.length > 10
+            ? data.results.slice(0,10)
+            : data.results;
+        if (results.length < data.results.length) {
+            console.log('%d of %d results ...', results.length, data.results.length);
+        }
+        results.forEach(function(r) {
             console.log(_(r).pluck('name').join(', '));
         });
+        console.warn(data.query);
+        console.warn(data.stats);
     });
 });
