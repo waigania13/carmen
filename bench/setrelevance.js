12ca7df943a5efd16121f3ed2d10281928757e17
Young Hahn <young@developmentseed.org>
2013-12-23 12:40:28 -0500

    Penalize results slightly based on gaps between db indices.

--- bench/setrelevance.js
+++ bench/setrelevance.js
@@ -7,7 +7,7 @@ suite.add('setrelevance', function() {
     // @TODO worst case scenario this data
     assert.equal(1, getSetRelevance(['georgia','vermont'], [
         { id: 3553, relev: 1, reason: 2, count: 1, idx: 1, db: 'province', tmpid: 100000000003553 },
-        { id: 130305, relev: 1, reason: 1, count: 1, idx: 3, db: 'place', tmpid: 300000000130305 }
+        { id: 130305, relev: 1, reason: 1, count: 1, idx: 2, db: 'place', tmpid: 300000000130305 }
     ]));
 })
 .on('cycle', function(event) {
--- test/setrelevance.test.js
+++ test/setrelevance.test.js
@@ -12,6 +12,11 @@ describe('getSetRelevance', function() {
         // Relev 1 match for 2 of 2 terms.
         assert.equal(1, getSetRelevance(['georgia','vermont'], [
             { id: 3553, relev: 1, reason: 2, count: 1, idx: 1, db: 'province', tmpid: 100000000003553 },
+            { id: 130305, relev: 1, reason: 1, count: 1, idx: 2, db: 'place', tmpid: 300000000130305 }
+        ]));
+        // Relev penalized for 2 of 2 terms, but with a gap in db index.
+        assert.equal(0.99, getSetRelevance(['georgia','vermont'], [
+            { id: 3553, relev: 1, reason: 2, count: 1, idx: 1, db: 'province', tmpid: 100000000003553 },
             { id: 130305, relev: 1, reason: 1, count: 1, idx: 3, db: 'place', tmpid: 300000000130305 }
         ]));
         // Second match for the same reason does not contribute to final relevance.
