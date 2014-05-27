f08ec4d22a15ea1a5464b13986ae9761a7d90bf6
ingalls <nick@mapbox.com>
2014-05-27 16:03:10 -0400

    Writing New Tests

--- test/geocode.test.js
+++ test/geocode.test.js
@@ -53,32 +53,28 @@ describe('geocode', function() {
             geocoder.geocode('saint john', { proximity: [98.177876]}, function(err, res) {    
                 if (!err || res)
                     assert.fail(err, "1Invalid Proximity did not throw");
-                else
-                    done();
+                done();
             });
     });
     it ('invalid proximity lat', function(done) {
             geocoder.geocode('n korea', { proximity: [98.177876,-59.504401]}, function(err, res) {
                 if (!err || res)
                     assert.fail(err, "2Invalid Proximity did not throw");
-                else
-                    done();;
+                done();;
             });
     });
     it ('invalid proximity lon', function(done) {
             geocoder.geocode('new york', { proximity: [58.177876,-200.504401]}, function(err, res) {
                 if (!err || res)
                     assert.fail(err, "3Invalid Proximity did not throw");
-                else
-                    done();
+                done();
             });
     });
     it ('text in proximity field', function(done) {
             geocoder.geocode('usa', { proximity: ["58d.177876","-200.5044s01"]}, function(err, res) {
                 if (!err || res)
                     assert.fail(err, "4Invalid Proximity did not throw");
-                else
-                    done();
+                done();
             });
     });
     it ('reverse', function(done) {
