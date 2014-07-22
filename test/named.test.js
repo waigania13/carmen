f62c9c2a15e5f6382115c947c47c0eb599715a3d
ingalls <nick@mapbox.com>
2014-07-22 09:47:04 -0400

    Tapify named.test

--- test/named.test.js
+++ test/named.test.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var assert = require('assert');
 var util = require('util');
 var Carmen = require('..');
 var S3 = require('tilelive-s3');
@@ -8,73 +7,79 @@ var feature = require('../lib/util/feature');
 var memFixture = require('./fixtures/mem.json');
 var mem = require('../lib/api-mem');
 var UPDATE = process.env.UPDATE;
+var test = require('tape');
 
-describe('index', function() {
-    var east = new S3(__dirname + '/fixtures/01-ne.country.east.s3');
-    var west = new S3(__dirname + '/fixtures/01-ne.country.west.s3');
-//    var eastSource = new mem(null, function() {});
-//    var westSource = new mem(null, function() {});
-//    eastSource.getIndexableDocs = function(pointer, callback) {
-//        mem.prototype.getIndexableDocs.call(this, pointer, function(err, docs, pointer) {
-//            if (err) return callback(err);
-//            docs = docs.filter(function(d) { return d._center[0] >= 0 });
-//            return callback(null, docs, pointer);
-//        });
-//    };
-//    westSource.getIndexableDocs = function(pointer, callback) {
-//        mem.prototype.getIndexableDocs.call(this, pointer, function(err, docs, pointer) {
-//            if (err) return callback(err);
-//            docs = docs.filter(function(d) { return d._center[0] < 0 });
-//            return callback(null, docs, pointer);
-//        });
-//    };
-//    it.skip('indexes east', function(done) {
-//        this.timeout(60e3);
-//        var c = new Carmen({ east:east, eastSource:eastSource });
-//        c.index(eastSource, east, {}, function(err) {
-//            assert.ifError(err);
-//            feature.getAllFeatures(east, function(err, features) {
-//                assert.ifError(err);
-//                assert.equal(features.length, 164);
-//                done();
-//            });
-//        });
-//    });
-//    it.skip('indexes west', function(done) {
-//        this.timeout(60e3);
-//        var c = new Carmen({ west:west, westSource:westSource });
-//        c.index(westSource, west, {}, function(err) {
-//            assert.ifError(err);
-//            feature.getAllFeatures(west, function(err, features) {
-//                assert.ifError(err);
-//                assert.equal(features.length, 89);
-//                done();
-//            });
-//        });
-//    });
-    it('queries east as country', function(done) {
+test('index', function(t) {
+    var east = new S3(__dirname + '/fixtures/01-ne.country.east.s3', function(err, source) {
+        if (err) t.fail();
+    });
+    var west = new S3(__dirname + '/fixtures/01-ne.country.west.s3', function(err, source) {
+        if (err) t.fail();
+    });
+    var eastSource = new mem(null, function() {});
+    var westSource = new mem(null, function() {});
+    eastSource.getIndexableDocs = function(pointer, callback) {
+        mem.prototype.getIndexableDocs.call(this, pointer, function(err, docs, pointer) {
+            if (err) return callback(err);
+            docs = docs.filter(function(d) { return d._center[0] >= 0 });
+            return callback(null, docs, pointer);
+        });
+    };
+    westSource.getIndexableDocs = function(pointer, callback) {
+        mem.prototype.getIndexableDocs.call(this, pointer, function(err, docs, pointer) {
+            if (err) return callback(err);
+            docs = docs.filter(function(d) { return d._center[0] < 0 });
+            return callback(null, docs, pointer);
+        });
+    };
+    t.skip('indexes east', function(q) {
+        this.timeout(60e3);
+        var c = new Carmen({ east:east, eastSource:eastSource });
+        c.index(eastSource, east, {}, function(err) {
+            q.ifError(err);
+            feature.getAllFeatures(east, function(err, features) {
+                q.ifError(err);
+                q.equal(features.length, 164);
+                q.end();
+            });
+        });
+    });
+    t.skip('indexes west', function(q) {
+        this.timeout(60e3);
+        var c = new Carmen({ west:west, westSource:westSource });
+        c.index(westSource, west, {}, function(err) {
+            q.ifError(err);
+            feature.getAllFeatures(west, function(err, features) {
+                q.ifError(err);
+                q.equal(features.length, 89);
+                q.end();
+            });
+        });
+    });
+    t.test('queries east as country', function(q) {
         var c = new Carmen({
             east: east,
             west: west
         });
         c.geocode('china', {}, function(err, res) {
-            assert.ifError(err);
-            assert.equal(res.features.length, 1);
-            assert.equal(res.features[0].id, 'country.3');
-            done();
+            q.ifError(err);
+            q.equal(res.features.length, 1);
+            q.equal(res.features[0].id, 'country.3');
+            q.end();
         });
     });
-    it('queries west as country', function(done) {
+    t.test('queries west as country', function(q) {
         var c = new Carmen({
             east: east,
             west: west
         });
         c.geocode('brazil', {}, function(err, res) {
-            assert.ifError(err);
-            assert.equal(res.features.length, 1);
-            assert.equal(res.features[0].id, 'country.7');
-            done();
+            q.ifError(err);
+            q.equal(res.features.length, 1);
+            q.equal(res.features[0].id, 'country.7');
+            q.end();
         });
     });
+    t.end();
 });
 
