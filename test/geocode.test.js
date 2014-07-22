8ef73ffc648ed4cf12cef2d47b978741983cb300
ingalls <nick@mapbox.com>
2014-07-22 10:51:40 -0400

    Tapify geocode.test

--- test/geocode.test.js
+++ test/geocode.test.js
@@ -1,98 +1,97 @@
 var fs = require('fs');
-var assert = require('assert');
 var util = require('util');
 var Carmen = require('..');
 var UPDATE = process.env.UPDATE;
+var test = require('tape');
 
-describe('geocode', function() {
+test('geocode', function(t) {
     var geocoder = new Carmen({
         country: Carmen.auto(__dirname + '/fixtures/01-ne.country.s3'),
         province: Carmen.auto(__dirname + '/fixtures/02-ne.province.s3')
     });
-    before(function(done) {
-        geocoder._open(done);
-    });
-    it ('forward', function(done) {
+
+    t.test ('forward', function(q) {
         geocoder.geocode('georgia', {}, function(err, res) {
-            assert.ifError(err);
+            q.ifError(err);
             if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/geocode-forward.json', JSON.stringify(res, null, 4));
-            assert.deepEqual(require(__dirname + '/fixtures/geocode-forward.json'), res);
-            done();
+            q.deepEqual(require(__dirname + '/fixtures/geocode-forward.json'), res);
+            q.end();
         });
     });
-    it ('forward + by id', function(done) {
+    t.test ('forward + by id', function(q) {
         geocoder.geocode('country.38', {}, function(err, res) {
-            assert.ifError(err);
+            q.ifError(err);
             if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/search-ident.json', JSON.stringify(res, null, 4));
-            assert.deepEqual(require(__dirname + '/fixtures/search-ident.json'), res);
-            done();
+            q.deepEqual(require(__dirname + '/fixtures/search-ident.json'), res);
+            q.end();
         });
     });
-    it ('forward + geocoder_tokens', function(done) {
+    t.test ('forward + geocoder_tokens', function(q) {
         geocoder.geocode('n korea', {}, function(err, res) {
-            assert.ifError(err);
+            q.ifError(err);
             if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/geocode-forward-tokens.json', JSON.stringify(res, null, 4));
-            assert.deepEqual(require(__dirname + '/fixtures/geocode-forward-tokens.json'), res);
-            done();
+            q.deepEqual(require(__dirname + '/fixtures/geocode-forward-tokens.json'), res);
+            q.end();
         });
     });
-    it ('proximity geocoding', function(done) {
+    t.test ('proximity geocoding', function(q) {
         geocoder.geocode('saint john', {}, function(err, res) {
-            assert.ifError(err);
+            q.ifError(err);
             if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/geocode-without-proximity.json', JSON.stringify(res, null, 4));
-            assert.equal(res.features[0].place_name, require(__dirname + '/fixtures/geocode-without-proximity.json').features[0].place_name, res);
+            q.equal(res.features[0].place_name, require(__dirname + '/fixtures/geocode-without-proximity.json').features[0].place_name, res);
             geocoder.geocode('saint john', { proximity: [13.177876,-59.504401]}, function(err, res) {
-                assert.ifError(err);
+                q.ifError(err);
                 if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/geocode-with-proximity.json', JSON.stringify(res, null, 4));
-                assert.equal(res.features[0].place_name, require(__dirname + '/fixtures/geocode-with-proximity.json').features[0].place_name, res);
-                done();
+                q.equal(res.features[0].place_name, require(__dirname + '/fixtures/geocode-with-proximity.json').features[0].place_name, res);
+                q.end();
             });
         });
     });
-    it ('string proximity geocoding', function(done) {
+    t.test ('string proximity geocoding', function(q) {
         geocoder.geocode('n korea', { proximity: "13.177876"}, function(err, res) {
-            assert.ifError(!err);
-            done();
+            q.ifError(!err);
+            q.end();
         });
     });
-    it ('invalid proximity length', function(done) {
-            geocoder.geocode('saint john', { proximity: [98.177876]}, function(err, res) {    
-                assert.ifError(!err);
-                done();
+    t.test ('invalid proximity length', function(q) {
+            geocoder.geocode('saint john', { proximity: [98.177876]}, function(err, res) {
+                q.ifError(!err);
+                q.end();
             });
     });
-    it ('invalid proximity lat', function(done) {
+    t.test ('invalid proximity lat', function(q) {
             geocoder.geocode('n korea', { proximity: [98.177876,-59.504401]}, function(err, res) {
-                assert.ifError(!err);
-                done();
+                q.ifError(!err);
+                q.end();
             });
     });
-    it ('invalid proximity lon', function(done) {
+    t.test ('invalid proximity lon', function(q) {
             geocoder.geocode('new york', { proximity: [58.177876,-200.504401]}, function(err, res) {
-                assert.ifError(!err);
-                done();
+                q.ifError(!err);
+                q.end();
             });
     });
-    it ('text in proximity field', function(done) {
+    t.test ('text in proximity field', function(q) {
             geocoder.geocode('usa', { proximity: ["58d.177876","-200.5044s01"]}, function(err, res) {
-                assert.ifError(!err);
-                done();
+                q.ifError(!err);
+                q.end();
             });
     });
-    it ('reverse', function(done) {
+    t.test ('reverse', function(q) {
         geocoder.geocode('0, 40', {}, function(err, res) {
-            assert.ifError(err);
+            q.ifError(err);
             if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/geocode-reverse.json', JSON.stringify(res, null, 4));
-            assert.deepEqual(require(__dirname + '/fixtures/geocode-reverse.json'), res);
-            done();
+            q.deepEqual(require(__dirname + '/fixtures/geocode-reverse.json'), res);
+            q.end();
         });
     });
-    it ('noresults', function(done) {
+    t.test ('noresults', function(q) {
         geocoder.geocode('asdfasdf', {}, function(err, res) {
-            assert.ifError(err);
+            q.ifError(err);
             if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/geocode-noresults.json', JSON.stringify(res, null, 4));
-            assert.deepEqual(require(__dirname + '/fixtures/geocode-noresults.json'), res);
-            done();
+            q.deepEqual(require(__dirname + '/fixtures/geocode-noresults.json'), res);
+            q.end();
         });
     });
+    t.end();
 });
