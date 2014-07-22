e4af83a6e0f23ec7d0c517275e63002f4c973746
ingalls <nick@mapbox.com>
2014-07-22 09:56:10 -0400

    tapify index.test

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var assert = require('assert');
 var util = require('util');
 var Carmen = require('..');
 var index = require('../lib/index');
@@ -7,99 +6,101 @@ var memFixture = require('./fixtures/mem.json');
 var MBTiles = require('mbtiles'),
     mem = require('../lib/api-mem');
 var UPDATE = process.env.UPDATE;
+var test = require('tape');
 
-describe('index.update', function() {
+test('index.update', function(t) {
     var to = new mem(null, function() {});
     var carmen = new Carmen({ to: to });
-    it('error no _id', function(done) {
+    t.test('error no _id', function(q) {
         index.update(to, [{_text:'main st'}], function(err) {
-            assert.equal('Error: doc has no _id', err.toString());
-            done();
+            q.equal('Error: doc has no _id', err.toString());
+            q.end();
         });
     });
-    it('error no _zxy', function(done) {
+    t.test('error no _zxy', function(q) {
         index.update(to, [{_text:'main st',_id:1}], function(err) {
-            assert.equal('Error: doc has no _zxy', err.toString());
-            done();
+            q.equal('Error: doc has no _zxy', err.toString());
+            q.end();
         });
     });
-    it('error no _zxy (empty array)', function(done) {
+    t.test('error no _zxy (empty array)', function(q) {
         index.update(to, [{_text:'main st',_id:1,_zxy:[]}], function(err) {
-            assert.equal('Error: doc has no _zxy', err.toString());
-            done();
+            q.equal('Error: doc has no _zxy', err.toString());
+            q.end();
         });
     });
-    it('error no _center', function(done) {
+    t.test('error no _center', function(q) {
         index.update(to, [{_text:'main st',_id:1,_zxy:['0/0/0']}], function(err) {
-            assert.equal('Error: doc has no _center', err.toString());
-            done();
+            q.equal('Error: doc has no _center', err.toString());
+            q.end();
         });
     });
-    it('indexes single doc', function(done) {
+    t.test('indexes single doc', function(q) {
         index.update(to, [{_text:'main st',_id:1,_zxy:['0/0/0'],_center:[0,0]}], function(err) {
-            assert.ifError(err);
-            done();
+            q.ifError(err);
+            q.end();
         });
     });
+    t.end();
 });
 
-describe('index', function() {
+test('index', function(t) {
     var from = new mem(null, function() {});
     var to = new mem(null, function() {});
     var carmen = new Carmen({
         from: from,
         to: to
     });
-    it('indexes a document', function(done) {
+    t.test('indexes a document', function(q) {
         carmen.index(from, to, {}, function(err) {
-            assert.ifError(err);
+            q.ifError(err);
             // Updates the mem.json fixture on disk.
             if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/mem.json', JSON.stringify(to.serialize(), null, 4));
-            assert.deepEqual(to.serialize(), memFixture);
-            done();
+            q.deepEqual(to.serialize(), memFixture);
+            q.end();
         });
     });
-    it('verifies index', function(done) {
+    t.test('verifies index', function(q) {
         carmen.verify(to, function(err, stats) {
-            assert.ifError(err);
-            assert.deepEqual({ relation: [ 'term', 'phrase' ], count: [ 261, 265 ] }, stats[0]);
-            assert.deepEqual({ relation: [ 'term', 'grid' ], count: [ 261, 265 ] }, stats[1]);
-            assert.deepEqual({ relation: [ 'phrase', 'freq' ], count: [ 265, 410 ] }, stats[2]);
-            done();
+            q.ifError(err);
+            q.deepEqual({ relation: [ 'term', 'phrase' ], count: [ 261, 265 ] }, stats[0]);
+            q.deepEqual({ relation: [ 'term', 'grid' ], count: [ 261, 265 ] }, stats[1]);
+            q.deepEqual({ relation: [ 'phrase', 'freq' ], count: [ 265, 410 ] }, stats[2]);
+            q.end();
         });
     });
-    it('analyzes index', function(done) {
+    t.test('analyzes index', function(q) {
         carmen.analyze(to, function(err, stats) {
-            assert.ifError(err);
+            q.ifError(err);
             // Updates the mem-analyze.json fixture on disk.
             if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/mem-analyze.json', JSON.stringify(stats, null, 4));
-            assert.deepEqual(require('./fixtures/mem-analyze.json'), stats);
-            done();
+            q.deepEqual(require('./fixtures/mem-analyze.json'), stats);
+            q.end();
         });
     });
-    it('loadall index', function(done) {
+    t.test('loadall index', function(q) {
         to._geocoder.unloadall('degen');
         to._geocoder.unloadall('term');
         to._geocoder.unloadall('phrase');
         to._geocoder.unloadall('grid');
-        assert.ok(!to._geocoder.has('degen', 0));
-        assert.ok(!to._geocoder.has('term', 0));
-        assert.ok(!to._geocoder.has('phrase', 0));
-        assert.ok(!to._geocoder.has('grid', 0));
+        q.ok(!to._geocoder.has('degen', 0));
+        q.ok(!to._geocoder.has('term', 0));
+        q.ok(!to._geocoder.has('phrase', 0));
+        q.ok(!to._geocoder.has('grid', 0));
         carmen.loadall(to, 1, function(err) {
-            assert.ifError(err);
-            assert.ok(to._geocoder.has('degen', 0));
-            assert.ok(to._geocoder.has('term', 0));
-            assert.ok(to._geocoder.has('phrase', 0));
-            assert.ok(to._geocoder.has('grid', 0));
-            done();
+            q.ifError(err);
+            q.ok(to._geocoder.has('degen', 0));
+            q.ok(to._geocoder.has('term', 0));
+            q.ok(to._geocoder.has('phrase', 0));
+            q.ok(to._geocoder.has('grid', 0));
+            q.end();
         });
     });
-    it('wipes index', function(done) {
+    t.test('wipes index', function(q) {
         carmen.wipe(to, function(err) {
-            assert.ifError(err);
+            q.ifError(err);
             carmen.verify(to, function(err, stats) {
-                assert.deepEqual({
+                q.deepEqual({
                     freq: { '0': '' },
                     term: { '0': '' },
                     phrase: { '0': '' },
@@ -107,8 +108,9 @@ describe('index', function() {
                     degen: { '0': '' },
                     feature: { '0': '{}', '1':'{}', '2':'{}', '3':'{}' }
                 }, to.serialize().shards);
-                done();
+                q.end();
             });
         });
     });
+    t.end();
 });
