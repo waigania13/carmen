8808631fdd1272e360ea84f977017d33021ed95b
ingalls <nick@mapbox.com>
2014-07-22 09:37:54 -0400

    Convert relevance to tape

--- test/relev.test.js
+++ test/relev.test.js
@@ -1,18 +1,15 @@
-var assert = require('assert'),
-    Relev = require('../lib/util/relev');
+var Relev = require('../lib/util/relev');
+var test = require('tape');
 
-describe('relev', function() {
-    describe('construction', function() {
-        it('is constructed', function() {
-            var r = new Relev(0, 1, 2, 1, 3, 'place', 'place', 5);
-            assert.equal(r.id, 0);
-            assert.equal(r.relev, 1);
-            assert.equal(r.reason, 2);
-            assert.equal(r.count, 1);
-            assert.equal(r.idx, 3);
-            assert.equal(r.dbid, 'place');
-            assert.equal(r.dbname, 'place');
-            assert.equal(r.tmpid, 5);
-        });
-    });
+test('relev', function(t) {
+    var r = new Relev(0, 1, 2, 1, 3, 'place', 'place', 5);
+    t.equal(r.id, 0);
+    t.equal(r.relev, 1);
+    t.equal(r.reason, 2);
+    t.equal(r.count, 1);
+    t.equal(r.idx, 3);
+    t.equal(r.dbid, 'place');
+    t.equal(r.dbname, 'place');
+    t.equal(r.tmpid, 5);
+    t.end();
 });
