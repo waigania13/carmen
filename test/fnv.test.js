1633b82ea889f5150f26598a427bf5c5f5f90570
ingalls <nick@mapbox.com>
2014-07-22 11:07:19 -0400

    Tapify fnv

--- test/fnv.test.js
+++ test/fnv.test.js
@@ -1,13 +1,12 @@
-var assert = require('assert'),
-    fnv1a = require('../lib/util/fnv');
+var fnv1a = require('../lib/util/fnv');
+var test = require('tape');
 
-describe('fnv1a', function() {
-    it('strings', function() {
-        assert.deepEqual(fnv1a('foo'), 2851307223);
-        assert.deepEqual(fnv1a('foo bar'), 1170285226);
-        assert.deepEqual(fnv1a('foo').toString(2), '10101001111100110111111011010111');
-        assert.deepEqual(fnv1a(''), 2166136261);
-        assert.deepEqual(fnv1a('foo', 24), 2851307008);
-        assert.deepEqual(fnv1a('foo', 24).toString(2),'10101001111100110111111000000000');
-    });
+test('fnv1a', function(t) {
+    t.deepEqual(fnv1a('foo'), 2851307223);
+    t.deepEqual(fnv1a('foo bar'), 1170285226);
+    t.deepEqual(fnv1a('foo').toString(2), '10101001111100110111111011010111');
+    t.deepEqual(fnv1a(''), 2166136261);
+    t.deepEqual(fnv1a('foo', 24), 2851307008);
+    t.deepEqual(fnv1a('foo', 24).toString(2),'10101001111100110111111000000000');
+    t.end();
 });
