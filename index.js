6845893ab4e04e292e3c2c8a42b373c3e4995640
ingalls <nick@mapbox.com>
2014-08-21 17:32:40 -0400

    Use single geocoder_address flag

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ function Geocoder(options) {
             if (names.indexOf(name) === -1) names.push(name);
             source._geocoder = source._geocoder || new Cache(key, +info.geocoder_shardlevel || 0);
             source._geocoder.geocoder_address = !!parseInt(info.geocoder_address||0,10);
-            source._geocoder.geocoder_address_points = !!parseInt(info.geocoder_address_points||0,10);
             source._geocoder.geocoder_layer = (info.geocoder_layer||'').split('.').shift();
             source._geocoder.geocoder_tokens = termops.tokenizeMapping(info.geocoder_tokens||{});
             source._geocoder.maxzoom = info.maxzoom;
