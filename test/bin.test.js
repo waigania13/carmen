bc013c58bd068150646b78d5d89e0793b8ce2af7
Young Hahn <young@mapbox.com>
2014-11-08 18:31:41 -0500

    Allow nonfile from/to for carmen-copy. Add bin tests.

--- test/bin.test.js
+++ test/bin.test.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var tape = require('tape');
 var exec = require('child_process').exec;
+var tmpdir = require('os').tmpdir();
 var bin = path.resolve(path.join(__dirname, '..', 'scripts'));
 var fixture = path.resolve(path.join(__dirname, '..', 'tiles'));
 
@@ -29,3 +30,29 @@ tape('bin/lookup query', function(t) {
         t.end();
     });
 });
+tape('bin/carmen-copy noargs', function(t) {
+    exec(bin + '/carmen-copy.js', function(err, stdout, stderr) {
+        t.equal(1, err.code);
+        t.equal("Usage: carmen-copy.js <from> <to>\n", stdout);
+        t.end();
+    });
+});
+tape('bin/carmen-copy 1arg', function(t) {
+    exec(bin + '/carmen-copy.js tiles/01-ne.country.mbtiles', function(err, stdout, stderr) {
+        t.equal(1, err.code);
+        t.equal("Usage: carmen-copy.js <from> <to>\n", stdout);
+        t.end();
+    });
+});
+tape('bin/carmen-copy', function(t) {
+    var dst = tmpdir + '/carmen-copy-test.mbtiles';
+    exec(bin + '/carmen-copy.js tiles/01-ne.country.mbtiles ' + dst, function(err, stdout, stderr) {
+        t.ifError(err);
+        t.equal(/Copying tiles\/01-ne\.country\.mbtiles/.test(stdout), true);
+        t.equal(/Done\./.test(stdout), true);
+        t.equal(fs.statSync(dst).size > 80e3, true);
+        t.equal(fs.unlinkSync(dst), undefined, 'cleanup');
+        t.end();
+    });
+});
+
