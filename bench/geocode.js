cdc4f74e0265aedeb15dbff38eea7ad446e6fc9f
Young Hahn <young@mapbox.com>
2014-12-07 11:52:03 -0500

    Harden up phrasematch/geocode benchmarks.

--- bench/geocode.js
+++ bench/geocode.js
@@ -6,26 +6,35 @@ var mem = require('../lib/api-mem');
 var tape = require('tape');
 
 (function() {
-    var conf = { street: new mem({ maxzoom:14 }, function() {}) };
+    var conf = { street: new mem({ maxzoom:14, geocoder_shardlevel:2 }, function() {}) };
     var c = new Carmen(conf);
 
     tape('setup', function(assert) {
+        var start = +new Date;
         // streetnames with "Lake" from TIGER
+        var seq = 1;
         var docs = require('fs').readFileSync(__dirname + '/fixtures/lake-streetnames.txt', 'utf8')
             .split('\n')
             .filter(function(text) { return !!text; })
-            .map(function(text, i) {
-                var lat = Math.random() * 180 - 90;
-                var lon = Math.random() * 360 - 180;
-                return {
-                    _id: i+1,
-                    _text: text,
-                    _center: [lon, lat],
-                    _geometry: { type:'Point', coordinates:[lon,lat] }
-                };
-            });
+            .slice(0,10000)
+            .reduce(function(memo, text) {
+                // generate between 1-100 features with this text.
+                var seed = Math.ceil(Math.random() * 100);
+                for (var i = 0; i < seed; i++) {
+                    var lat = Math.random() * 180 - 90;
+                    var lon = Math.random() * 360 - 180;
+                    memo.push({
+                        _id: ++seq,
+                        _text: text,
+                        _center: [lon, lat],
+                        _geometry: { type:'Point', coordinates:[lon,lat] }
+                    });
+                }
+                return memo;
+            }, []);
         index.update(conf.street, docs, 14, function(err) {
             if (err) throw err;
+            assert.ok(true, 'setup time ' + (+new Date - start) + 'ms');
             assert.end();
         });
     });
@@ -36,7 +45,11 @@ var tape = require('tape');
         for (var i = 0; i < runs; i++) q.defer(doit);
         function doit(done) {
             c.geocode('Lake View Rd', {}, function (err, res) {
-                done();
+                if (!result.length) {
+                    done(new Error('No results'));
+                } else {
+                    done();
+                }
             });
         }
         q.awaitAll(function(err) {
