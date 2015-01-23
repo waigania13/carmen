8ed07e6c8f85a76bc8e4653c62bddfc89d627e5b
ingalls <nick@mapbox.com>
2015-01-23 16:00:46 -0500

    Fixed travelled logic bug

--- test/address.test.js
+++ test/address.test.js
@@ -2,63 +2,36 @@ var address = require('../lib/pure/applyaddress.js');
 var addressCluster = require('../lib/pure/addresscluster.js');
 var test = require('tape');
 
+//@TODO
+// point off the start poitn should be traelled 0
+// Point off the end point should be travelled = lineDistance
+
 test('address.getReversePoint', function(assert) {
-    assert.deepEquals(
-        address.getReversePoint([-77.19941765069962,38.94769891171966], [[-77.19998896121979,38.9475549770314],[-77.1988570690155,38.94759252523913]], 'miles'),
-        {
-            along: 691.1503837897545,
-            endLine: {
-                geometry: {
-                    coordinates: [0, 10],
-                    type: 'Point'
-                },
-                properties: {},
-                type: 'Feature'
-            },
-            pt: {
-                geometry: {
-                    coordinates: [1.1102230246251565e-16, 4.989053398093076],
-                    type: 'Point'
-                },
-                properties: {
-                    dist: 68.85675979958184,
-                    index: 0
-                },
-                type: 'Feature'
-            },
-            startLine: {
-                geometry: {
-                    coordinates: [0,0],
-                    type: 'Point'
-                },
-                properties: {},
-                type: 'Feature'
-            }
-        });
-    assert.deepEquals(
-        address.getReversePoint([1,12], [[0,0],[0,10]], 'miles'),
-        {
-            endLine: {
-                geometry: {
-                    coordinates: [ 0, 10 ],
-                    type: 'Point' },
-                properties: {},
-                type: 'Feature' },
-            pt: {
-                geometry: {
-                    coordinates: [ 0, 10 ],
-                    type: 'Point' },
-                properties: {
-                    dist: 153.98052056503332,
-                    index: 0 },
-                type: 'Feature' },
-            along: 691.1503837897545,
-            startLine: {
-                geometry: {
-                    coordinates: [ 0, 0 ],
-                    type: 'Point' },
-                properties: {},
-                type: 'Feature' } });
+    assert.deepEqual(
+        address.getReversePoint([-77.19932645559311,38.94770308373527], [[-77.19998091459274,38.9475549770314],[-77.19883829355238,38.94759461125006]], 'miles'),
+        { endLine: { geometry: { coordinates: [ -77.19883829355238, 38.94759461125006 ], type: 'Point' }, properties: {}, type: 'Feature' }, lineDist: 0.06147950790879972, pt: { geometry: { coordinates: [ -77.19932038464033, 38.94757788890475 ], type: 'Point' }, properties: { dist: 0.008658996759104212, index: 0, travelled: 0.03554027081555206 }, type: 'Feature' }, startLine: { geometry: { coordinates: [ -77.19998091459274, 38.9475549770314 ], type: 'Point' }, properties: {}, type: 'Feature' } },
+        "left centre side of line"
+    );
+
+    assert.deepEqual(
+        address.getReversePoint([-77.1995061635971,38.94741938611567], [[-77.19998091459274,38.9475549770314],[-77.19883829355238,38.94759461125006]], 'miles'),
+        { endLine: { geometry: { coordinates: [ -77.19883829355238, 38.94759461125006 ], type: 'Point' }, properties: {}, type: 'Feature' }, lineDist: 0.06147950790879972, pt: { geometry: { coordinates: [ -77.1995173939109, 38.947571055223044 ], type: 'Point' }, properties: { dist: 0.010499982934844238, index: 0, travelled: 0.024940051989783316 }, type: 'Feature' }, startLine: { geometry: { coordinates: [ -77.19998091459274, 38.9475549770314 ], type: 'Point' }, properties: {}, type: 'Feature' } },
+        "right centre side of line");
+
+    assert.deepEqual(
+        address.getReversePoint([-77.20057904720306,38.94761547135627], [[-77.19998091459274,38.9475549770314],[-77.19883829355238,38.94759461125006]], 'miles'),
+        { endLine: { geometry: { coordinates: [ -77.19883829355238, 38.94759461125006 ], type: 'Point' }, properties: {}, type: 'Feature' }, lineDist: 0.06147950790879972, pt: { geometry: { coordinates: [ -77.19998091459274, 38.9475549770314 ], type: 'Point' }, properties: { dist: 0.03242169132910228, index: 0, travelled: 0 }, type: 'Feature' }, startLine: { geometry: { coordinates: [ -77.19998091459274, 38.9475549770314 ], type: 'Point' }, properties: {}, type: 'Feature' } },
+        "before start of line");
+
+    assert.deepEqual(
+        address.getReversePoint([-77.19858080148697,38.94759461125006], [[-77.19998091459274,38.9475549770314],[-77.19883829355238,38.94759461125006]], 'miles'),
+        { endLine: { geometry: { coordinates: [ -77.19883829355238, 38.94759461125006 ], type: 'Point' }, properties: {}, type: 'Feature' }, lineDist: 0.06147950790879972, pt: { geometry: { coordinates: [ -77.19883829355238, 38.94759461125006 ], type: 'Point' }, properties: { dist: 0.013840773622621594, index: 0, travelled: 0.06147950790879972 }, type: 'Feature' }, startLine: { geometry: { coordinates: [ -77.19998091459274, 38.9475549770314 ], type: 'Point' }, properties: {}, type: 'Feature' } },
+        "after end of line");
+
+    // assert.deepEqual(
+    //     address.getReversePoint([-77.19932645559311,38.94770308373527], [[-77.19998091459274,38.9475549770314],[-77.19883829355238,38.94759461125006]], 'miles'),
+    //     null);
+
     assert.end();
 });
 
