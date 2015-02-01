509893625ddfc2c8f2c70552066431bd1edc68d7
ingalls <nick@mapbox.com>
2015-02-01 14:01:44 -0500

    Test vtile address

--- test/geocode-vtile.test.js
+++ test/geocode-vtile.test.js
@@ -17,14 +17,13 @@ mapnik.register_datasource(path.join(mapnik.settings.paths.input_plugins,'ogr.in
 (function() {
     var conf = { address: new mem({geocoder_address: 1, maxzoom: 14, format: 'pbf'}, function() {}) };
     var c = new Carmen(conf);
-
     test('Index Cluster', function(t) {
         var vtile = new mapnik.VectorTile(14,3640,5670);
         var address = {
             _id:1,
             _geometry: {
-                    "type": "LineString",
-                    "coordinates": [[ -100.00605583190918,48.36314970496242],[-100.00185012817383,48.36640011246755]]
+                    type: "LineString",
+                    coordinates: [[ -100.00605583190918,48.36314970496242],[-100.00185012817383,48.36640011246755]]
                 },
             _text:'fake street',
             _center: [-100.00605583190918,48.36314970496242],
@@ -37,13 +36,10 @@ mapnik.register_datasource(path.join(mapnik.settings.paths.input_plugins,'ogr.in
         zlib.gzip(vtile.getData(), function(err, buffer) {
             t.ifError(err, 'vtile gzip success');
             conf.address.putTile(14,3640,5670, buffer, function() {
-                index.update(conf.address, [address], 14, function() {
-                    t.end();
-                });
+                index.update(conf.address, [address], 14, t.end);
             });
         });
     });
-
     test('contextVector reverse Cluster', function (t) {
         c.geocode('-100.00185012817383,48.36640011246755', { limit_verify:1 }, function(err, res) {
             t.ifError(err);
