ddb15f7eb946682c7ae447096582b3399c4b765f
ingalls <nick@mapbox.com>
2015-02-07 19:25:13 -0500

    Remove context utf tests

--- test/context.test.js
+++ test/context.test.js
@@ -21,45 +21,18 @@ test('context vector', function(t) {
         t.test('context vt full', function(q) {
             context(geocoder, 0, 40, null, true, function(err, contexts) {
                 q.ifError(err);
-                q.equal(2, contexts.length);
+                q.equal(contexts.length, 2);
                 if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/context-vt-full.json', JSON.stringify(contexts, null, 4));
-                q.deepEqual(require(__dirname + '/fixtures/context-vt-full.json'), contexts);
+                q.deepEqual(contexts, require(__dirname + '/fixtures/context-vt-full.json'));
                 q.end();
             });
         });
         t.test('context vt light', function(q) {
             context(geocoder, 0, 40, null, false, function(err, contexts) {
                 q.ifError(err);
-                q.equal(2, contexts.length);
+                q.equal(contexts.length, 2);
                 if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/context-vt-light.json', JSON.stringify(contexts, null, 4));
-                q.deepEqual(require(__dirname + '/fixtures/context-vt-light.json'), contexts);
-                q.end();
-            });
-        });
-    });
-});
-
-test('context utf', function(t) {
-    var geocoder = new Carmen({
-        country: Carmen.auto(__dirname + '/fixtures/01-ne.country.utf.s3')
-    });
-
-    geocoder._open(function() {
-        t.test('context utf full', function(q) {
-            context(geocoder, 0, 40, null, true, function(err, contexts) {
-                q.ifError(err);
-                q.equal(1, contexts.length);
-                if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/context-utf-full.json', JSON.stringify(contexts, null, 4));
-                q.deepEqual(require(__dirname + '/fixtures/context-utf-full.json'), contexts);
-                q.end();
-            });
-        });
-        t.test('context utf light', function(q) {
-            context(geocoder, 0, 40, null, false, function(err, contexts) {
-                q.ifError(err);
-                q.equal(1, contexts.length);
-                if (UPDATE) fs.writeFileSync(__dirname + '/fixtures/context-utf-light.json', JSON.stringify(contexts, null, 4));
-                q.deepEqual(require(__dirname + '/fixtures/context-utf-light.json'), contexts);
+                q.deepEqual(contexts, require(__dirname + '/fixtures/context-vt-light.json'));
                 q.end();
             });
         });
