311747ae24397634253ab109b94ad5bb5cd8d675
Young Hahn <young@mapbox.com>
2015-04-05 12:15:29 -0400

    Switch getIndexableText to consume a full doc rather than just text.

--- test/termops.test.js
+++ test/termops.test.js
@@ -110,24 +110,42 @@ test('termops', function(t) {
 
 test('termops.getIndexableText', function(assert) {
     var freq = { 0:[2] };
-    assert.deepEqual(termops.getIndexableText(token.createReplacer({}), 'Main Street'), [
+    var replacer;
+    var doc;
+
+    replacer = token.createReplacer({});
+    doc = {_text:'Main Street'};
+    assert.deepEqual(termops.getIndexableText(replacer, doc), [
         [ 'main', 'street' ]
     ], 'creates indexableText');
-    assert.deepEqual(termops.getIndexableText(token.createReplacer({'Street':'St'}), 'Main Street'), [
+
+    replacer = token.createReplacer({'Street':'St'});
+    doc = {_text:'Main Street'};
+    assert.deepEqual(termops.getIndexableText(replacer, doc), [
         [ 'main', 'street' ],
         [ 'main', 'st' ]
     ], 'creates contracted phrases using geocoder_tokens');
-    assert.deepEqual(termops.getIndexableText(token.createReplacer({'Street':'St'}), 'Main Street, main st'), [
+
+    replacer = token.createReplacer({'Street':'St'});
+    doc = {_text:'Main Street, main st'};
+    assert.deepEqual(termops.getIndexableText(replacer, doc), [
         [ 'main', 'street' ],
         [ 'main', 'st' ]
     ], 'dedupes phrases');
-    assert.deepEqual(termops.getIndexableText(token.createReplacer({'Street':'St', 'Lane':'Ln'}), 'Main Street Lane'), [
+
+    replacer = token.createReplacer({'Street':'St', 'Lane':'Ln'});
+    doc = {_text:'Main Street Lane'};
+    assert.deepEqual(termops.getIndexableText(replacer, doc), [
         [ 'main', 'street', 'lane' ],
         [ 'main', 'st', 'ln' ]
     ], 'dedupes phrases');
-    assert.deepEqual(termops.getIndexableText(token.createReplacer({'dix-huitième':'18e'}), 'Avenue du dix-huitième régiment'), [
+
+    replacer = token.createReplacer({'dix-huitième':'18e'});
+    doc = {_text:'Avenue du dix-huitième régiment'};
+    assert.deepEqual(termops.getIndexableText(replacer, doc), [
         [ 'avenue', 'du', 'dix', 'huitieme', 'regiment' ],
         [ 'avenue', 'du', '18e', 'regiment' ]
     ], 'hypenated replacement');
+
     assert.end();
 });
