3e400fba01b1ab2aa4218c492f7db98614891918
Young Hahn <young@mapbox.com>
2015-04-07 13:02:58 -0400

    Refactor encoding of terms to preserve numeric values.

--- test/termops.encode.test.js
+++ test/termops.encode.test.js
@@ -2,9 +2,29 @@ var termops = require('../lib/util/termops');
 var token = require('../lib/util/token');
 var test = require('tape');
 
+test('termops.encodeDegen', function(assert) {
+    assert.deepEqual(termops.encodeDegen('main'), 3935363584, 'encodes term');
+    assert.deepEqual(termops.encodeDegen('{"type":"range","min":539,"max":550}'), false, 'does not encode dataterm');
+
+    assert.deepEqual(termops.encodeDegen('1234'), 1234 * 16, 'encodes numeric term');
+    assert.deepEqual(termops.encodeDegen('1234', 1), 1234 * 16 + 1, 'encodes numeric term');
+
+    assert.deepEqual(termops.encodeDegen('2345b'), 2345 * 16, 'encodes seminumeric term');
+    assert.deepEqual(termops.encodeDegen('2345b', 1), 2345 * 16 + 1, 'encodes seminumeric term');
+
+    assert.end();
+});
+
 test('termops.encodeTerm', function(assert) {
     assert.deepEqual(termops.encodeTerm('main'), 3935363584, 'encodes term');
     assert.deepEqual(termops.encodeTerm('{"type":"range","min":539,"max":550}'), 4503747267009280 + 1, 'encodes dataterm with weight 1');
+
+    assert.deepEqual(termops.encodeTerm('1234'), 1234 * 16, 'encodes numeric term');
+    assert.deepEqual(termops.encodeTerm('1234', 1), 1234 * 16 + 1, 'encodes numeric term');
+
+    assert.deepEqual(termops.encodeTerm('2345b'), 2345 * 16, 'encodes seminumeric term');
+    assert.deepEqual(termops.encodeTerm('2345b', 1), 2345 * 16 + 1, 'encodes seminumeric term');
+
     assert.end();
 });
 
