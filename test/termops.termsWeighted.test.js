a1fe6e405f3fbc433d6f3a7c732fadd11216e8c1
Young Hahn <young@mapbox.com>
2015-04-13 18:32:28 -0400

    Disallow weightless terms

---
+++ test/termops.termsWeighted.test.js
@@ -0,0 +1,42 @@
+var termops = require('../lib/util/termops');
+var token = require('../lib/util/token');
+var test = require('tape');
+
+test('termops.termsWeighted', function(assert) {
+    var res;
+
+    res = termops.termsWeighted([
+        'a',
+        'b',
+        'c'
+    ], {
+        0:[10003],
+        3826002208:[10000],
+        3876335072:[1],
+        3859557456:[1]
+    });
+    assert.deepEqual(res, [
+        3826002208 + 1,
+        3876335072 + 15,
+        3859557456 + 15
+    ], 'weights terms');
+
+    res = termops.termsWeighted([
+        'a',
+        'b',
+        'c'
+    ], {
+        0:[1000],
+        3826002208:[10000],
+        3876335072:[1],
+        3859557456:[1]
+    });
+    assert.deepEqual(res, [
+        3826002208 + 1,
+        3876335072 + 15,
+        3859557456 + 15
+    ], 'weights > 0 even if freq drops below total');
+
+    assert.end();
+});
+
