755f09cf63fb8eebfb8bab027cbf5e9b7fa49ebf
Young Hahn <young@mapbox.com>
2015-05-26 13:09:59 -0400

    Sort by score, type, distance, until a combined heuristic can be tested.

--- test/verifymatch.test.js
+++ test/verifymatch.test.js
@@ -21,38 +21,45 @@ tape('verifymatch.sortContext (no distance)', function(assert) {
     var c;
     var arr = [];
 
-    c = [{ _id: 9 }];
+    c = [{ _id: 10 }];
     c._relevance = 0.9;
     arr.push(c);
 
-    c = [{ _id: 8, _address:'26' }];
+    c = [{ _id: 9, _address:'26' }];
     c._relevance = 1.0;
     arr.push(c);
 
-    c = [{ _id: 7, _address:'26', _cluster:{}, _geometry: { omitted: true } }];
+    c = [{ _id: 8, _address:'26', _cluster:{}, _geometry: { omitted: true } }];
     c._relevance = 1.0;
     arr.push(c);
 
-    c = [{ _id: 6, _address:'26', _cluster:{}, _geometry: {} }];
+    c = [{ _id: 7, _address:'26', _cluster:{}, _geometry: {} }];
     c._relevance = 1.0;
     arr.push(c);
 
-    c = [{ _id: 5, _address:'26', _cluster:{}, _geometry: {}, _score:1 }];
+    c = [{ _id: 6, _address:'26', _cluster:{}, _geometry: {}, _score:1 }];
+    c._relevance = 1.0;
+    arr.push(c);
+
+    c = [{ _id: 5, _address:'26', _cluster:{}, _geometry: {}, _score:2 }];
     c._relevance = 1.0;
     arr.push(c);
 
     c = [{ _id: 4, _address:'26', _cluster:{}, _geometry: {}, _score:2 }];
     c._relevance = 1.0;
+    c._typeindex = 2;
     arr.push(c);
 
     c = [{ _id: 3, _address:'26', _cluster:{}, _geometry: {}, _score:2 }];
     c._relevance = 1.0;
-    c._typeindex = 2;
+    c._typeindex = 1;
+    c._distance = 20;
     arr.push(c);
 
     c = [{ _id: 2, _address:'26', _cluster:{}, _geometry: {}, _score:2 }];
     c._relevance = 1.0;
     c._typeindex = 1;
+    c._distance = 10;
     arr.push(c);
 
     c = [{ _id: 1, _address:'26', _cluster:{}, _geometry: {}, _score:2, _position: 2 }];
@@ -66,7 +73,7 @@ tape('verifymatch.sortContext (no distance)', function(assert) {
     arr.push(c);
 
     arr.sort(verifymatch.sortContext);
-    assert.deepEqual(arr.map(function(c) { return c[0]._id }), [0,1,2,3,4,5,6,7,8,9]);
+    assert.deepEqual(arr.map(function(c) { return c[0]._id }), [0,1,2,3,4,5,6,7,8,9,10]);
 
     assert.end();
 });
