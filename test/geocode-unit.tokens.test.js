4bfa343c6a71a68d11785a0d05eac4f9b48b23a4
Tom Lee <thomas.j.lee@gmail.com>
2015-06-08 14:01:02 -0400

    enable regex token replacement
    don't test relevance, just presence of result
    
    don't use a query that splits into tokens

--- test/geocode-unit.tokens.test.js
+++ test/geocode-unit.tokens.test.js
@@ -67,8 +67,38 @@ var addFeature = require('./util/addfeature');
     });
 })();
 
+(function() {
+    var conf = {
+        address: new mem({
+            maxzoom: 6,
+            geocoder_tokens: {'q(?<x>[a-z])(?<y>[a-z])(?<z>[a-z])': "${z}${y}${x}"}
+        }, function() {})
+    };
+    var c = new Carmen(conf);
+    tape('geocoder token test', function(t) {
+        var address = {
+            _id:1,
+            _text:'cba',
+            _zxy:['6/32/32'],
+            _center:[0,0],
+            _geometry: {
+                type: "Point",
+                coordinates: [0,0]
+            }
+        };
+        addFeature(conf.address, address, t.end);
+    });
+    tape('test token replacement', function(t) {
+        c.geocode('qabc', { limit_verify: 1 }, function (err, res) {
+            t.ifError(err);
+            console.log(res);
+            t.equals(res.features[0].relevance, 0.99, 'token regex named group test, a-b-c => cba');
+            t.end();
+        });
+    });
+})();
+
 tape('index.teardown', function(assert) {
     index.teardown();
     assert.end();
 });
-
