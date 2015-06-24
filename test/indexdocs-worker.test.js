cf04432d07da101562283d1e800cf57e05d0ac55
Young Hahn <young@mapbox.com>
2015-06-24 10:28:47 -0400

    Sketch of new indexing process [ci skip]

--- test/indexdocs-worker.test.js
+++ test/indexdocs-worker.test.js
@@ -1,6 +1,51 @@
 var worker = require('../lib/indexer/indexdocs-worker.js');
+var grid = require('../lib/util/grid.js');
 var tape = require('tape');
 var termops = require('../lib/util/termops.js');
+var token = require('../lib/util/token.js');
+
+tape('worker.loadDoc', function(assert) {
+    var token_replacer = token.createReplacer({});
+    var patch;
+    var tokens;
+    var freq;
+    var zoom;
+    var doc;
+    var err;
+
+    patch = { grid:{}, docs:[] };
+    freq = {};
+    tokens = ['main', 'st'];
+    zoom = 6;
+    doc = {
+        _id: 1,
+        _text: 'main st',
+        _center: [0, 0],
+        _zxy: ['6/32/32'],
+        _score: 7
+    };
+
+    freq[0] = [101];
+    freq[termops.encodeTerm(tokens[0])] = [1];
+    freq[termops.encodeTerm(tokens[1])] = [100];
+
+    // Indexes single doc.
+    err = worker.loadDoc(patch, doc, freq, zoom, token_replacer);
+    assert.ifError(err);
+    assert.deepEqual(Object.keys(patch.grid).length, 7);
+    assert.deepEqual(patch.grid[Object.keys(patch.grid)[0]].length, 1);
+    assert.deepEqual(grid.decode(patch.grid[Object.keys(patch.grid)[0]][0]), {
+        id: 1,
+        relev: 1,
+        score: 7,
+        x: 32,
+        y: 32
+    });
+    assert.deepEqual(patch.docs.length, 1);
+    assert.deepEqual(patch.docs[0], doc);
+
+    assert.end();
+});
 
 tape('worker.verifyCenter', function(assert) {
     assert.equal(worker.verifyCenter([0,0], [[0,0,0]]), true, 'center in tiles');
