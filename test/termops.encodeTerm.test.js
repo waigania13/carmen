d6741e80d43460b6d1485dc2f35264fd5eee3c2c
Young Hahn <young@mapbox.com>
2015-07-02 23:15:40 -0400

    Get autocomplete for queries like '### 14' working.

--- test/termops.encodeTerm.test.js
+++ test/termops.encodeTerm.test.js
@@ -5,7 +5,8 @@ var test = require('tape');
 test('termops.encodeTerm', function(assert) {
     assert.deepEqual(termops.encodeTerm('main'), 3935363592, 'encodes term');
     assert.deepEqual(termops.encodeTerm('1234'), 4257489661, 'encodes numeric term');
-    assert.deepEqual(termops.encodeTerm('2345b'), 784195493, 'encodes seminumeric term');
+    assert.deepEqual(termops.encodeTerm('2345b'), 1541883461, 'encodes seminumeric term');
+    assert.deepEqual(termops.encodeTerm('2345'), 784195493, 'encodes seminumeric differently from numeric term');
     assert.deepEqual(termops.encodeTerm('LS24'), 651597038, 'encodes non-address numeric term with fnv1a');
     assert.end();
 });
