b0d633ef913eb6078b799c906619ecb4f8bdf720
Young Hahn <young@mapbox.com>
2015-07-08 17:13:28 -0400

    Turn it up to ... 5

--- test/copy.test.js
+++ test/copy.test.js
@@ -65,3 +65,53 @@ test('copy', function(t) {
 
     t.end();
 });
+
+// Tests copy @ shardlevel 5
+test('copy (shardlevel=5)', function(t) {
+    var from = new mem({geocoder_shardlevel:5}, function() {});
+    var to = new mem({geocoder_shardlevel:5}, function() {});
+    var carmen = new Carmen({
+        from: from,
+        to: to
+    });
+    var zoom = 6;
+
+    t.test('update', function(q) {
+        index.update(from, docs, zoom, function(err) {
+            if (err) q.fail();
+            index.store(from, function(){
+                q.end();
+            });
+        });
+    });
+
+    t.test('blank', function(q) {
+        carmen.analyze(to, function(err, stats) {
+            q.ifError(err);
+            q.deepEqual(stats, {
+                byRelev: { '0.4': 0, '0.6': 0, '0.8': 0, '1.0': 0 },
+                byScore: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 },
+                degen: 0,
+                ender: 0,
+                shardlevel: 5,
+                total: 0
+            });
+            q.end();
+        });
+    });
+
+    t.test('copies', function(q) {
+        carmen.copy(from, to, function(err) {
+            q.ifError(err);
+            q.end();
+        });
+    });
+
+    t.test('index.teardown', function(assert) {
+        index.teardown();
+        assert.end();
+    });
+
+    t.end();
+});
+
