d82e8a02054609efabefa7cb0c7be29251bf52e0
Young Hahn <young@mapbox.com>
2015-07-15 11:29:42 -0400

    Comments, tweaks.

--- test/proximity.test.js
+++ test/proximity.test.js
@@ -8,29 +8,30 @@ test('proximity.center2zxy', function(assert) {
 });
 
 test('proximity._scoredist', function(assert) {
-    assert.equal(proximity._scoredist(1, 80.0), 1, '80.0 miles => 1');
-    assert.equal(proximity._scoredist(1, 40.0), 2, '40.0 miles => 2');
-    assert.equal(proximity._scoredist(1, 20.0), 4, '20.0 miles => 4');
-    assert.equal(proximity._scoredist(1, 8.0),  10,   '8.0 miles => 10');
-    assert.equal(proximity._scoredist(1, 4.0),  20,   '4.0 miles => 20');
-    assert.equal(proximity._scoredist(1, 2.0),  40,   '2.0 miles => 40');
-    assert.equal(proximity._scoredist(1, 1.0),  80,   '1.0 miles => 80');
-    assert.equal(proximity._scoredist(1, 0.2),  400,  '0.2 miles => 400');
-    assert.equal(proximity._scoredist(1, 0.1),  800,  '0.1 miles => 800');
-    assert.equal(proximity._scoredist(1, 0.0),  800000, '0.0 miles => 800000');
+    assert.equal(proximity._scoredist(1, 80.0), 0.5, '80.0 miles');
+    assert.equal(proximity._scoredist(1, 40.0), 1, '40.0 miles');
+    assert.equal(proximity._scoredist(1, 20.0), 2, '20.0 miles');
+    assert.equal(proximity._scoredist(1, 8.0),  5,   '8.0 miles');
+    assert.equal(proximity._scoredist(1, 5.0),  8,   '5.0 miles = break even pt');
+    assert.equal(proximity._scoredist(1, 4.0),  10,   '4.0 miles');
+    assert.equal(proximity._scoredist(1, 2.0),  20,   '2.0 miles');
+    assert.equal(proximity._scoredist(1, 1.0),  40,   '1.0 miles');
+    assert.equal(proximity._scoredist(1, 0.2),  200,  '0.2 miles');
+    assert.equal(proximity._scoredist(1, 0.1),  400,  '0.1 miles');
+    assert.equal(proximity._scoredist(1, 0.0),  400000, '0.0 miles');
     assert.end();
 });
 
 test('proximity.scoredist', function(assert) {
     // lon/lat center
-    assert.equal(proximity.scoredist([0,0], [0,0], 1000), 8e8, 'll scoredist');
-    assert.equal(proximity.scoredist([0,0], [0.05,0], 1000), 23149.8099, 'll scoredist');
-    assert.equal(proximity.scoredist([0,0], [2.00,0], 1000), 578.7452, 'll scoredist');
-    assert.equal(proximity.scoredist([0,0], [10.00,0], 1000), 115.749, 'll scoredist');
+    assert.equal(proximity.scoredist([0,0], [0,0], 1000), 4e8, 'll scoredist');
+    assert.equal(proximity.scoredist([0,0], [0.05,0], 1000), 11574.905, 'll scoredist');
+    assert.equal(proximity.scoredist([0,0], [2.00,0], 1000), 289.3726, 'll scoredist');
+    assert.equal(proximity.scoredist([0,0], [10.00,0], 1000), 57.8745, 'll scoredist');
 
     // zxy center
-    assert.equal(proximity.scoredist([0,0], [0,0,0], 1000), 8e8, 'zxy scoredist');
-    assert.equal(proximity.scoredist([0,0], [1,0,0], 1000), 12.861, 'zxy scoredist');
+    assert.equal(proximity.scoredist([0,0], [0,0,0], 1000), 4e8, 'zxy scoredist');
+    assert.equal(proximity.scoredist([0,0], [1,0,0], 1000), 6.4305, 'zxy scoredist');
     assert.equal(proximity.scoredist([0,0], [1,0,0], 1000), proximity.scoredist([0,0], [-90,45], 1000), 'zxy scoredist ~= ll scoredist');
 
     assert.end();
