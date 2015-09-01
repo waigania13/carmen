51e7b2a791869e8d6a0fb307ec91e49b96ca4fb0
ingalls <nick@mapbox.com>
2015-09-01 14:57:45 +0000

    Fix ops tests

--- test/ops.test.js
+++ test/ops.test.js
@@ -2,30 +2,33 @@ var ops = require('../lib/util/ops');
 var test = require('tape');
 
 test('ops#toFeature', function(t) {
-    t.deepEqual(ops.toFeature([
-        {
-            "_center": [-99.392855, 63.004759],
-            "_text": "Canada, CA",
-            "_extid": "country.1833980151",
-            "_relevance": 1
+    t.deepEqual(ops.toFeature([{
+        properties: {
+            "carmen:center": [-99.392855, 63.004759],
+            "carmen:text": "Canada, CA",
+            "carmen:extid": "country.1833980151",
+            "carmen:relevance": 1
         }
-        ]), {
-            id: 'country.1833980151',
-            type: 'Feature',
+    }]), {
+        id: 'country.1833980151',
+        type: 'Feature',
+        properties: {
             text: 'Canada',
             place_name: 'Canada',
             relevance: undefined,
-            center: [ -99.392855, 63.004759 ],
-            geometry: { type: 'Point', coordinates: [ -99.392855, 63.004759 ] },
-            properties: {}
+            center: [ -99.392855, 63.004759 ]
+        },
+        geometry: { type: 'Point', coordinates: [ -99.392855, 63.004759 ] }
     });
 
     t.deepEqual(ops.toFeature([{
-            "_center": [-99.392855,63.004759],
-            "_address": 9,
-            "_text": "Fake Street",
-            "_extid": "country.1833980151",
-            "_relevance": 1
-        }], "{name} {num}"), { address: 9, center: [ -99.392855, 63.004759 ], geometry: { coordinates: [ -99.392855, 63.004759 ], type: 'Point' }, id: 'country.1833980151', place_name: 'Fake Street 9', properties: {}, relevance: undefined, text: 'Fake Street', type: 'Feature' });
+        properties: {
+            "carmen:center": [-99.392855,63.004759],
+            "carmen:address": 9,
+            "carmen:text": "Fake Street",
+            "carmen:extid": "country.1833980151",
+            "carmen:relevance": 1
+        }
+    }], "{name} {num}"), { geometry: { coordinates: [ -99.392855, 63.004759 ], type: 'Point' }, id: 'country.1833980151', properties: { address: 9, center: [ -99.392855, 63.004759 ], place_name: 'Fake Street 9', relevance: undefined, text: 'Fake Street' }, type: 'Feature' });
     t.end();
 });
