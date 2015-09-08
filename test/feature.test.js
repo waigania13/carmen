822c6a2c905732bc3fd541ebc3d85f063c7bbc31
ingalls <nick@mapbox.com>
2015-09-08 17:42:08 +0000

    asserts for feature#transform

--- test/feature.test.js
+++ test/feature.test.js
@@ -27,3 +27,114 @@ tape('shard', function(assert) {
     }
     assert.end();
 });
+
+tape('transform', function(assert) {
+
+    assert.test('transform - basic feature', function(t) {
+        var feat = feature.transform({
+            _id: 7654,
+            _text: "Main Street",
+            _geometry: {
+                type: "MultiPoint",
+                coordinates: [[ -97, 37 ], [ -97.2, 37 ]]
+            }
+        });
+        t.deepEquals(feat, { 
+            id: 7654,
+            type: 'Feature',
+            properties: {
+                'carmen:text': 'Main Street'
+            },
+            geometry: {
+                coordinates: [ [ -97, 37 ], [ -97.2, 37 ] ],
+                type: 'MultiPoint'
+            }
+        });
+        t.end();
+    });
+
+    assert.test('transform - zxy feature', function(t) {
+        var feat = feature.transform({
+            _id: 7654,
+            _text: "Main Street",
+            _zxy: ['6/32/32']
+        });
+        t.deepEquals(feat, {
+            id:7654,
+            type: "Feature",
+            properties: {
+                "carmen:zxy": ['6/32/32'],
+                "carmen:text": "Main Street"
+            },
+            geometry: {
+                type:"MultiPolygon",
+                coordinates: [[[[0,-5.615985819155337],[0,0],[5.625,0],[5.625,-5.615985819155337],[0,-5.615985819155337]]]]
+            }
+        });
+        t.end();
+    });
+    
+    assert.test('transform - cluster', function(t) {
+        var feat = feature.transform({
+            _id: 7654,
+            _text: 'Main Street',
+            _cluster: { 2: { type: "Point", coordinates: [0,0] } }
+        });
+        t.deepEquals(feat, {
+            id: 7654,
+            type: "Feature",
+            properties: {
+                'carmen:addressnumber': [ '2' ],
+                'carmen:text': 'Main Street'
+            },
+            geometry: {
+                type: 'MultiPoint',
+                coordinates: [[0,0]]
+            }
+        });
+        t.end();
+    });
+    
+    assert.test('transform - range', function(t) {
+        var feat = feature.transform({
+            _id: "7654",
+            _text: "Main Street",
+            _center: [ -97.1, 37 ],
+            _score: 99,
+            _rangetype: "tiger",
+            _lfromhn: [ "100", "200" ],
+            _ltohn: ["198", "298"],
+            _rfromhn: ["101", "201"],
+            _rtohn: ["199", "299"],
+            _parityl: ["E", "E"],
+            _parityr: ["O", "B"],
+            _geometry: {
+                type: "MultiLineString",
+                coordinates: [
+                    [[ -97, 37 ],[ -97.2, 37 ],[ -97.2, 37.2 ]],
+                    [[ -97.2, 37.2 ],[ -97.4, 37.2 ],[ -97.4, 37.4 ]]
+                ]
+            }
+        });
+        t.deepEquals(feat,  { 
+            geometry: {
+                coordinates: [ [ [ -97, 37 ], [ -97.2, 37 ], [ -97.2, 37.2 ] ], [ [ -97.2, 37.2 ], [ -97.4, 37.2 ], [ -97.4, 37.4 ] ] ], 
+                type: 'MultiLineString'
+            },
+            id: '7654',
+            properties: {
+                'carmen:center': [ -97.1, 37 ],
+                'carmen:lfromhn': [ '100', '200' ],
+                'carmen:ltohn': [ '198', '298' ],
+                'carmen:parityl': [ 'E', 'E' ],
+                'carmen:parityr': [ 'O', 'B' ],
+                'carmen:rangetype': 'tiger',
+                'carmen:rfromhn': [ '101', '201' ],
+                'carmen:rtohn': [ '199', '299' ],
+                'carmen:score': 99,
+                'carmen:text': 'Main Street'
+            }, 
+            type: 'Feature' });
+        t.end();
+    });
+});
