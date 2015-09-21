74aeea6a1d007aa39baca19ade36dd3c7d66956b
Camille Teicheira <cteicheira@gmail.com>
2015-09-21 16:28:37 -0700

    try to store previous values

--- bench/decollide.js
+++ bench/decollide.js
@@ -2,6 +2,11 @@ var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite();
 var assert = require('assert');
 var termops = require('../lib/util/termops');
+var fs = require('fs');
+var UPDATE = process.env.UPDATE;
+
+var expected = require('./expected/decollide');
+var values = {};
 
 suite
 .add('decollide (collision)', function() {
@@ -15,6 +20,16 @@ suite
     }, 'av francisco de aguirre'), true);
 })
 .on('cycle', function(event) {
+    var name = event.target.name;
+    values[name] = event.target.hz;
     console.log(String(event.target));
+    var compare = values[name]/expected[name];
+    assert.ok(compare > 0.85);
+    if (compare >= 1) console.log('  ', ((compare - 1) * 100).toFixed(2) + '%', 'faster than previously recorded');
+    else console.log('  ', ((1 - compare) * 100).toFixed(2) + '%', 'slower than previously recorded');
+
+})
+.on('complete', function() {
+    if (UPDATE) fs.writeFileSync(__dirname+'/expected/decollide.json', JSON.stringify(values, null, 2));
 })
 .run();
