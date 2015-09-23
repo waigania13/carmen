279d43f8bec1ef526df4d8d6a0ff1ffa080e91d9
Camille Teicheira <cteicheira@gmail.com>
2015-09-23 15:00:56 -0700

    refactor so benchmarks can be run as a suite

--- bench/decollide.js
+++ bench/decollide.js
@@ -4,32 +4,45 @@ var assert = require('assert');
 var termops = require('../lib/util/termops');
 var fs = require('fs');
 var UPDATE = process.env.UPDATE;
+var ss = require('simple-statistics');
 
 var expected = require('./expected/decollide');
 var values = {};
 
-suite
-.add('decollide (collision)', function() {
-    assert.equal(termops.decollide([], {
-        properties: { 'carmen:text': 'r ademar da silva neiva #'}
-    }, 'av francisco de aguirre # la serena'), false);
-})
-.add('decollide (clean)', function() {
-    assert.equal(termops.decollide([], {
-        properties: { 'carmen:text': 'av francisco de aguirre #'}
-    }, 'av francisco de aguirre'), true);
-})
-.on('cycle', function(event) {
-    var name = event.target.name;
-    values[name] = event.target.hz;
-    console.log(String(event.target));
-    var compare = values[name]/expected[name];
-    assert.ok(compare > 0.85);
-    if (compare >= 1) console.log('  ', ((compare - 1) * 100).toFixed(2) + '%', 'faster than previously recorded');
-    else console.log('  ', ((1 - compare) * 100).toFixed(2) + '%', 'slower than previously recorded');
+module.exports = benchmark;
 
-})
-.on('complete', function() {
-    if (UPDATE) fs.writeFileSync(__dirname+'/expected/decollide.json', JSON.stringify(values, null, 2));
-})
-.run();
+function benchmark(cb) {
+    if (!cb) cb = function(){};
+    console.log('# decollide');
+
+    suite
+    .add('decollide (collision)', function() {
+        assert.equal(termops.decollide([], {
+            properties: { 'carmen:text': 'r ademar da silva neiva #'}
+        }, 'av francisco de aguirre # la serena'), false);
+    })
+    .add('decollide (clean)', function() {
+        assert.equal(termops.decollide([], {
+            properties: { 'carmen:text': 'av francisco de aguirre #'}
+        }, 'av francisco de aguirre'), true);
+    })
+    .on('cycle', function(event) {
+        var name = event.target.name;
+        values[name] = event.target.hz;
+        console.log(String(event.target));
+
+        var compare = values[name]/expected[name];
+        // assert.ok(compare > 0.85);
+        if (compare >= 1) console.log('  ', ((compare - 1) * 100).toFixed(2) + '%', 'faster than previously recorded');
+        else console.log('  ', ((1 - compare) * 100).toFixed(2) + '%', 'slower than previously recorded');
+    })
+    .on('complete', function() {
+        console.log();
+        if (UPDATE) fs.writeFileSync(__dirname+'/expected/decollide.json', JSON.stringify(values, null, 2));
+        cb();
+    })
+    .run();
+}
+
+if (!process.env.runSuite) benchmark();
+
--- bench/verifymatch.verifyFeatures.js
+++ bench/verifymatch.verifyFeatures.js
@@ -1,6 +1,5 @@
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite();
-var assert = require('assert');
 var verifymatch = require('../lib/verifymatch');
 var loaded = require('./fixtures/verifymatch.verifyFeatures.loaded.json');
 var spatial = require('./fixtures/verifymatch.verifyFeatures.spatial.json');
@@ -8,28 +7,41 @@ var tokens = require('./fixtures/tokens.json');
 var token = require('../lib/util/token');
 var replacer = token.createReplacer(tokens);
 
-suite.add('index', function() {
-    var l = JSON.parse(JSON.stringify(loaded));
-    var query = ['1600','p'];
-    var geocoder = { byidx: {} };
+module.exports = benchmark;
 
-    for (var i = 0; i < 40; i++) geocoder.byidx[i] = {
-        _geocoder: {
-            token_replacer: replacer,
-            geocoder_address: true
-        }
-    };
-    var options = {
-        proximity: [ -77.0414, 38.9004 ],
-        debug: false,
-        stats: true,
-        limit: 5,
-        allow_dupes: false,
-        limit_verify: 10
-    };
-    var ver = verifymatch.verifyFeatures(query, geocoder, spatial, l, options);
-})
-.on('cycle', function(event) {
-    console.log(String(event.target));
-})
-.run();
+function benchmark(cb) {
+    if (!cb) cb = function(){};
+    console.log('# verifymatch.verifyFeatures');
+
+    suite.add('index', function() {
+        var l = JSON.parse(JSON.stringify(loaded));
+        var query = ['1600','p'];
+        var geocoder = { byidx: {} };
+
+        for (var i = 0; i < 40; i++) geocoder.byidx[i] = {
+            _geocoder: {
+                token_replacer: replacer,
+                geocoder_address: true
+            }
+        };
+        var options = {
+            proximity: [ -77.0414, 38.9004 ],
+            debug: false,
+            stats: true,
+            limit: 5,
+            allow_dupes: false,
+            limit_verify: 10
+        };
+        verifymatch.verifyFeatures(query, geocoder, spatial, l, options);
+    })
+    .on('cycle', function(event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function() {
+      console.log();
+      cb();
+    })
+    .run();
+}
+
+if (!process.env.runSuite) benchmark();
