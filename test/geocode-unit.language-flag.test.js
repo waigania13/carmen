6bdbb5a2496c5c313aae7c1ff2d099694ccdbd9b
Camille Teicheira <cteicheira@gmail.com>
2015-10-06 17:42:07 -0700

    add basic test

---
+++ test/geocode-unit.language-flag.test.js
@@ -0,0 +1,110 @@
+//Ensure that results that have equal relev in phrasematch
+//are matched against the 0.5 relev bar instead of 0.75
+
+var tape = require('tape');
+var Carmen = require('..');
+var index = require('../lib/index');
+var mem = require('../lib/api-mem');
+var queue = require('queue-async');
+var addFeature = require('../lib/util/addfeature');
+
+var conf = {
+    country: new mem({ maxzoom:6 }, function() {})
+};
+var c = new Carmen(conf);
+
+tape('index country', function(t) {
+    var country = {
+        type: 'Feature',
+        properties: {
+            'carmen:center': [ 0, 0 ],
+            'carmen:zxy': [ '6/30/30' ],
+            'carmen:text': 'Russian Federation, Rossiyskaya Federatsiya',
+            'carmen:text_ru': 'Российская Федерация',
+            'carmen:text_zh_Latn': 'Elousi',
+            'carmen:text_fake': 'beetlejuice',
+            'carmen:text_es': null
+        },
+        id: 2,
+        geometry: { type: 'MultiPolygon', coordinates: [] },
+        bbox: [ -11.25, 5.615, -5.625, 11.1784 ]
+    };
+    addFeature(conf.country, country, t.end);
+});
+
+tape('russia => Russian Federation', function(t) {
+    c.geocode('russia', { limit_verify:1 }, function(err, res) {
+        t.ifError(err);
+        t.deepEqual(res.features[0].place_name, 'Russian Federation');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.end();
+    });
+});
+
+tape('Rossiyskaya => Russian Federation', function(t) {
+    c.geocode('Rossiyskaya', { limit_verify:1 }, function(err, res) {
+        t.ifError(err);
+        t.deepEqual(res.features[0].place_name, 'Russian Federation');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.end();
+    });
+});
+
+tape('Rossiyskaya => Российская Федерация - {language: "ru"}', function(t) {
+    c.geocode('Rossiyskaya', { limit_verify:1, language: 'ru' }, function(err, res) {
+        t.ifError(err);
+        t.deepEqual(res.features[0].place_name, 'Российская Федерация');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.end();
+    });
+});
+
+// 'fake' is not a valid language code
+tape('Rossiyskaya => Russian Federation - {language: "fake"}', function(t) {
+    c.geocode('Rossiyskaya', { limit_verify:1, language: 'fake' }, function(err, res) {
+        t.ifError(err);
+        t.deepEqual(res.features[0].place_name, 'Russian Federation');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.end();
+    });
+});
+
+// no value for 'es'
+tape('Rossiyskaya => Russian Federation - {language: "es"}', function(t) {
+    c.geocode('Rossiyskaya', { limit_verify:1, language: 'es' }, function(err, res) {
+        t.ifError(err);
+        t.deepEqual(res.features[0].place_name, 'Russian Federation');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.end();
+    });
+});
+
+// no value for 'en'
+tape('Rossiyskaya => Russian Federation - {language: "en"}', function(t) {
+    c.geocode('Rossiyskaya', { limit_verify:1, language: 'en' }, function(err, res) {
+        t.ifError(err);
+        t.deepEqual(res.features[0].place_name, 'Russian Federation');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.deepEqual(res.features[0].id, 'country.2');
+        t.end();
+    });
+});
+
+//Is not above 0.5 relev so should fail.
+tape('fake blah blah => [fail]', function(t) {
+    c.geocode('fake blah blah', { limit_verify:1 }, function(err, res) {
+        t.ifError(err);
+        t.notOk(res.features[0]);
+        t.end();
+    });
+});
+
+tape('index.teardown', function(assert) {
+    index.teardown();
+    assert.end();
+});
