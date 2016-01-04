8a579571c370779d5680aa859ea6b7dc0954385d
ingalls <nicholas.ingalls@gmail.com>
2016-01-04 17:38:21 -0500

    First pass at streaming GeoJSON indexer

--- scripts/carmen-index.js
+++ scripts/carmen-index.js
@@ -4,22 +4,62 @@ var fs = require('fs');
 var path = require('path');
 var argv = process.argv;
 var Carmen = require('../index.js');
-var f = argv[2];
-var t = argv[3];
+var argv = require('minimist')(process.argv, {
+    string: [ 'version', 'config', 'input', 'output', 'index' ],
+    boolean: [ 'help' ]
+});
+var settings = require('../package.json');
 
-if (!f) {
-    console.warn('Usage: carmen-index.js <from> [to]');
-    process.exit(1);
+if (argv.help) {
+    console.log('carmen-copy.js --config=<path> --index=<path> [options]');
+    console.log('[options]:');
+    console.log('  --help                  Prints this message');
+    console.log('  --version               Print the carmen version');
+    console.log('  --config="<path>"       Path to JSON document with index settings');
+	console.log('  --index="<path>"        Tilelive path to output index to');
+    console.log('  --ouput="<path>"        Path to output Normalized GeoJSON to');
+    console.log('                            If unset outputs to STDOUT');
+    console.log('  --input="<path>"        File containing GeoJSON FeatureCollection to index');
+    console.log('                            If unset waits for FeatureCollection from STDIN');
+
+    process.exit(0);
 }
 
-var nogrids = ('NOGRIDS' in process.env);
-if (nogrids) console.log('Indexing without grids.');
+if (argv.version) {
+    console.log('carmen@'+settings.version);
+    process.exit(0);
+}
 
-console.log('Indexing %s ...', f);
+if (!argv.config) throw new Error('--settings argument required');
 
-var conf = {};
-conf.from = Carmen.auto(f);
-conf.to = t ? Carmen.auto(t) : from;
+var inputStream; //STDIN (default) or fs readStream w/ GeoJSON FeatureCollection
+var outputStream; //STOUT (default) or fs writeStream
+
+if (argv.output) {
+    outputStream = fs.createWriteStream(path.resolve(process.cwd(), argv.output));
+} else {
+    outputStream = process.stdout;
+}
+
+if (argv.input) {
+    inputStream = fs.createReadStream(path.resolve(process.cwd(), argv._[0]), { encoding: 'utf8' });
+} else {
+    process.stdin.setEncoding('utf8');
+    process.stdin.resume();
+    inputStream = process.stdin;
+}
+
+argv.index = Carmen.auto(argv.index);
+
+var conf = {
+	config: require(), //TODO require JSON index config
+	index: argv.index,
+	input: inputStream,
+	ouput: outputStream
+};
+
+
+//OLD CODE TO REVIEW
 
 var carmen = new Carmen(conf);
 var last = +new Date;
