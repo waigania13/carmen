308270d64a97cb7e11397b3e0bfabbcb607f17d9
ingalls <nicholas.ingalls@gmail.com>
2016-01-07 15:22:12 -0500

    Wait for carmen init in io test

--- test/geocode-unit.io-stack.test.js
+++ test/geocode-unit.io-stack.test.js
@@ -19,58 +19,60 @@ var conf = {
     street3: new mem({ maxzoom:6, geocoder_name: 'street', timeout:10, geocoder_address:1 }, function() {})
 };
 var c = new Carmen(conf);
-[1,2,3].forEach(function(i) {
-    tape('index place ' + i, function(t) {
-        addFeature(conf['place'+i], {
-            _id:1,
-            _text:'springfield',
-            _zxy:['6/32/32'],
-            _center:[0,0]
-        }, t.end);
+c.on('open', function() {
+    [1,2,3].forEach(function(i) {
+        tape('index place ' + i, function(t) {
+            addFeature(conf['place'+i], {
+                _id:1,
+                _text:'springfield',
+                _zxy:['6/32/32'],
+                _center:[0,0]
+            }, t.end);
+        });
+        tape('index street ' + i, function(t) {
+            addFeature(conf['street'+i], {
+                _id:1,
+                _text:'winding river rd',
+                _zxy:['6/32/32'],
+                _center:[0,0]
+            }, t.end);
+        });
+        tape('index street ' + i, function(t) {
+            addFeature(conf['street'+i], {
+                _id:2,
+                _text:'river rd',
+                _zxy:['6/32/32'],
+                _center:[0,0]
+            }, t.end);
+        });
+        tape('clear memory, i/o log', function(t) {
+            conf['place'+i]._geocoder.unloadall('grid');
+            conf['place'+i]._original.logs.getGeocoderData = [];
+            conf['place'+i]._original.logs.getTile = [];
+            conf['street'+i]._geocoder.unloadall('grid');
+            conf['street'+i]._original.logs.getGeocoderData = [];
+            conf['street'+i]._original.logs.getTile = [];
+            t.end();
+        });
     });
-    tape('index street ' + i, function(t) {
-        addFeature(conf['street'+i], {
-            _id:1,
-            _text:'winding river rd',
-            _zxy:['6/32/32'],
-            _center:[0,0]
-        }, t.end);
-    });
-    tape('index street ' + i, function(t) {
-        addFeature(conf['street'+i], {
-            _id:2,
-            _text:'river rd',
-            _zxy:['6/32/32'],
-            _center:[0,0]
-        }, t.end);
-    });
-    tape('clear memory, i/o log', function(t) {
-        conf['place'+i]._geocoder.unloadall('grid');
-        conf['place'+i]._original.logs.getGeocoderData = [];
-        conf['place'+i]._original.logs.getTile = [];
-        conf['street'+i]._geocoder.unloadall('grid');
-        conf['street'+i]._original.logs.getGeocoderData = [];
-        conf['street'+i]._original.logs.getTile = [];
-        t.end();
-    });
-});
 
-tape('winding river rd springfield', function(t) {
-    c.geocode('winding river rd  springfield', {}, function(err, res) {
-        t.ifError(err);
-        t.deepEqual(res.features[0].place_name, 'winding river rd, springfield');
-        t.deepEqual(c.indexes.place1._original.logs.getGeocoderData, ['grid,62799'], 'place1: loads 1 grid');
-        t.deepEqual(c.indexes.place1._original.logs.getTile, ['6,32,32'], 'place1: loads 1 tile');
+    tape('winding river rd springfield', function(t) {
+        c.geocode('winding river rd  springfield', {}, function(err, res) {
+            t.ifError(err);
+            t.deepEqual(res.features[0].place_name, 'winding river rd, springfield');
+            t.deepEqual(c.indexes.place1._original.logs.getGeocoderData, ['grid,62799'], 'place1: loads 1 grid');
+            t.deepEqual(c.indexes.place1._original.logs.getTile, ['6,32,32'], 'place1: loads 1 tile');
 
-        t.deepEqual(c.indexes.street1._original.logs.getGeocoderData.sort(), ['feature,1','feature,2','grid,52975','grid,8765'], 'street1: loads 1 grid, 1 feature per result');
-        t.deepEqual(c.indexes.street1._original.logs.getTile, [], 'street1: loads no tiles (most specific index)');
-        t.end();
+            t.deepEqual(c.indexes.street1._original.logs.getGeocoderData.sort(), ['feature,1','feature,2','grid,52975','grid,8765'], 'street1: loads 1 grid, 1 feature per result');
+            t.deepEqual(c.indexes.street1._original.logs.getTile, [], 'street1: loads no tiles (most specific index)');
+            t.end();
+        });
     });
-});
 
-tape('index.teardown', function(assert) {
-    index.teardown();
-    context.getTile.cache.reset();
-    assert.end();
+    tape('index.teardown', function(assert) {
+        index.teardown();
+        context.getTile.cache.reset();
+        assert.end();
+    });
 });
 
