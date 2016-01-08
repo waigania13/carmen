93c1467b2f8dd8f0269f2bfa6e8db6b463729b54
ingalls <nicholas.ingalls@gmail.com>
2016-01-08 11:37:57 -0500

    remove input/output opts

--- scripts/carmen-index.js
+++ scripts/carmen-index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var argv = process.argv;
 var Carmen = require('../index.js');
 var argv = require('minimist')(process.argv, {
-    string: [ 'version', 'config', 'input', 'output', 'index' ],
+    string: [ 'version', 'config', 'index' ],
     boolean: [ 'help' ]
 });
 var settings = require('../package.json');
@@ -17,10 +17,7 @@ if (argv.help) {
     console.log('  --version               Print the carmen version');
     console.log('  --config="<path>"       Path to JSON document with index settings');
 	console.log('  --index="<path>"        Tilelive path to output index to');
-    console.log('  --ouput="<path>"        Path to output Normalized GeoJSON to');
-    console.log('                            If unset outputs to STDOUT');
-    console.log('  --input="<path>"        File containing GeoJSON FeatureCollection to index');
-    console.log('                            If unset waits for FeatureCollection from STDIN');
+    console.log('');
 	console.log('Deprecated:');
     console.log('carmen-copy.js [from] [to]');
     process.exit(0);
@@ -36,22 +33,11 @@ if (argv.version) {
 if (!argv._[2]) {
 	if (!argv.config) throw new Error('--settings argument required');
 
-	var inputStream; //STDIN (default) or fs readStream w/ GeoJSON FeatureCollection
-	var outputStream; //STOUT (default) or fs writeStream
+    var outputStream = process.stdout;
 
-	if (argv.output) {
-		outputStream = fs.createWriteStream(path.resolve(process.cwd(), argv.output));
-	} else {
-		outputStream = process.stdout;
-	}
-
-	if (argv.input) {
-		inputStream = fs.createReadStream(path.resolve(process.cwd(), argv._[0]), { encoding: 'utf8' });
-	} else {
-		process.stdin.setEncoding('utf8');
-		process.stdin.resume();
-		inputStream = process.stdin;
-	}
+    process.stdin.setEncoding('utf8');
+    process.stdin.resume();
+    var inputStream = process.stdin;
 
 	argv.index = Carmen.auto(argv.index);
 
@@ -61,7 +47,12 @@ if (!argv._[2]) {
 
 	var carmen = new Carmen(conf);
 
-	carmen.index(null, conf.index, {}, complete);
+	carmen.index(null, conf.index, {
+        input: inputStream,
+        output: outputStream,
+        config: config
+
+    }, complete);
 } else {
 	//Legacy Indexer
 	if (!argv._[2]) throw new Error('[From] argument required');
