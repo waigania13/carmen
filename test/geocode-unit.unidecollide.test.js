5e27a6031e7a951b301a211772bbe1978be8c919
Tom Lee <thomas.j.lee@gmail.com>
2016-02-20 12:33:30 -0500

    test for unidecode collision

---
+++ test/geocode-unit.unidecollide.test.js
@@ -0,0 +1,59 @@
+// test separation of character sets, avoiding unidecode problems like:
+// 'Alberta' aka 'アルバータ州' =[unidecode]=> 'arubataZhou' => false positives for 'Aruba'
+
+var tape = require('tape');
+var Carmen = require('..');
+var index = require('../lib/index');
+var context = require('../lib/context');
+var mem = require('../lib/api-mem');
+var addFeature = require('../lib/util/addfeature');
+
+var conf = {
+    place_a: new mem({maxzoom:6, geocoder_name:'region'}, function() {}),
+};
+var c = new Carmen(conf);
+tape('index Alberta', function(t) {
+    addFeature(conf.place_a, {
+        id:1,
+        properties: {
+            'carmen:text':'Alberta',
+            'carmen:text_ja':'アルバータ州',
+            'carmen:zxy':['6/32/32'],
+            'carmen:center':[0,0]
+        }
+    }, t.end);
+});
+
+tape('heading to Aruba, I hope you packed warm clothes', function(t) {
+    c.geocode('aruba', { limit_verify:1 }, function(err, res) {
+        t.ifError(err);
+        t.equals(res.features.length, 0, 'Alberta feature does not match \'Aruba\'');
+        t.end();
+    });
+});
+
+tape('JP query works', function(t) {
+    c.geocode('アルバータ州', { limit_verify:1 }, function(err, res) {
+        t.ifError(err);
+        t.deepEqual(res.features[0].place_name, 'Alberta');
+        t.deepEqual(res.features[0].id, 'region.1');
+        t.end();
+    });
+});
+
+tape('Latin query works', function(t) {
+    c.geocode('Alber', { limit_verify:1 }, function(err, res) {
+        t.ifError(err);
+        t.deepEqual(res.features[0].place_name, 'Alberta');
+        t.deepEqual(res.features[0].id, 'region.1');
+        t.end();
+    });
+});
+
+
+tape('index.teardown', function(assert) {
+    index.teardown();
+    context.getTile.cache.reset();
+    assert.end();
+});
+
