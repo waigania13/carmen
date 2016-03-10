3a4ddb8cee60869ace7c09995bb1f7e5d3ef6d0c
Young Hahn <young@mapbox.com>
2016-03-10 10:54:36 -0500

    Add test for rebalance copying subqueries rather than adjusting by reference

--- test/spatialmatch.rebalance.test.js
+++ test/spatialmatch.rebalance.test.js
@@ -59,3 +59,47 @@ test('rebalance, with garbage', function(assert) {
     assert.end();
 });
 
+test('rebalance copies', function(assert) {
+    var query = ['100','main','st','12345','seattle','washington'];
+
+    var stackA = [];
+
+    stackA[0] = ['1##','main','st'];
+    stackA[0].mask = 7;
+    stackA[0].weight = 0.5;
+
+    stackA[1] = ['12345'];
+    stackA[1].mask = 8;
+    stackA[1].weight = 0.16666666666666666;
+
+    stackA[2] = ['seattle'];
+    stackA[2].mask = 16;
+    stackA[2].weight = 0.16666666666666666;
+
+    stackA[3] = ['washington'];
+    stackA[3].mask = 32;
+    stackA[3].weight = 0.16666666666666666;
+
+    stackA.relev = 1;
+
+    var stackB = [];
+    stackB[0] = stackA[0];
+
+    var rebalancedA = rebalance(query, stackA);
+    var rebalancedB = rebalance(query, stackB);
+
+    // Assert that the subqueries in rebalancedA are not affected by
+    // the rebalance done to rebalancedB.
+    assert.equal(rebalancedA.relev, 1, 'relev = 1');
+    assert.equal(rebalancedA[0].weight, 0.25, 'weight = 0.25');
+    assert.equal(rebalancedA[1].weight, 0.25, 'weight = 0.25');
+    assert.equal(rebalancedA[2].weight, 0.25, 'weight = 0.25');
+    assert.equal(rebalancedA[3].weight, 0.25, 'weight = 0.25');
+
+    // Vice versa.
+    assert.equal(rebalancedB.relev, 0.50, 'relev = 0.50');
+    assert.equal(rebalancedB[0].weight, 0.50, 'weight = 0.50');
+
+    assert.end();
+});
+
