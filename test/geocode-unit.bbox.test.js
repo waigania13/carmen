ab78a6e36289376b62b6ce0caec78e97e5cf4a2e
Matt Ficke <matt@mattficke.com>
2016-04-11 14:55:22 -0400

    add bbox check to verifymatch

--- test/geocode-unit.bbox.test.js
+++ test/geocode-unit.bbox.test.js
@@ -27,7 +27,7 @@ tape('index feature', function(t) {
     var feature = {
         id:2,
         properties: {
-            'carmen:text':'Main St',
+            'carmen:text':'Main Street',
             'carmen:zxy':['6/32/32'],
             'carmen:center':[0,0]
         }
@@ -35,18 +35,28 @@ tape('index feature', function(t) {
     addFeature(conf.street, feature, t.end);
 });
 
+// run query with invalid bbox, expect error
+tape('fake bbox', function(t) {
+    c.geocode('Main St', {bbox: [-1.0, -1.0, 1.0]}, function(err, res) {
+        t.equal(err && err.code, 'EINVALID');
+        t.end();
+    });
+});
+
 // run query without bbox filter, expect both features back
 tape('no bbox', function(t) {
     c.geocode('Main St', {}, function(err, res) {
         t.ifError(err);
         t.equals(res.features.length, 2);
+        t.end();
     });
 });
 
 // run query with bbox fitler, expect only one feature back
 tape('with bbox', function(t) {
-    c.geocode('Main St', {bbox: [-1.0, -1.0, 1.0, 1.0]}, function(err, res) {
+    c.geocode('Main St', { bbox: [-1.0, -1.0, 1.0, 1.0]}, function(err, res) {
         t.ifError(err);
         t.equals(res.features.length, 1);
+        t.end();
     });
 });
