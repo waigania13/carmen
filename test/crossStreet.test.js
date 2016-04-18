c2621a90eb240d89fbf56f8a2687a467113c3a51
Kai <riselikethemoon@gmail.com>
2016-04-18 10:29:36 -0400

    write test for combining crossStreet features

---
+++ test/crossStreet.test.js
@@ -0,0 +1,40 @@
+var crossStreets = require('../lib/verifymatch.js').crossStreets;
+var tape = require('tape');
+
+
+
+tape('Find intersection of two line features', function(assert) {
+    // given the geometries of two line features, return a feature matching the intersection
+    // currently returns a feature with combined name only
+    var feature1 = {
+        id:1,
+        properties: {
+            'carmen:text': 'fake street',
+            'carmen:center': [5,0],
+        },
+        geometry: {
+            type: 'LineString',
+            coordinates: [[5,0],[5,10]]
+        }
+    };
+    var feature2 = {
+        id:2,
+        properties: {
+            'carmen:text': 'main street',
+            'carmen:center': [0,5],
+        },
+        geometry: {
+            type: 'LineString',
+            coordinates: [[0,5],[10,5]]
+        }
+    };
+
+    var features = [feature1, feature2];
+    var result = crossStreets(features);
+    console.log(result);
+
+    assert.equals(result[0]['properties']['carmen:text'], 'fake street and main street');
+    assert.equals(result[0]['geometry']['coordinates'], [5, 5]);
+
+    assert.end();
+});
