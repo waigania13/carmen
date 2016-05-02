e902d85c53a9f4ffa77aaa19e7f881954563bbe4
kai <kai@mapbox.com>
2016-05-02 14:12:18 -0700

    Tidy, add test for two touching LineFeatures

--- test/crossStreet.test.js
+++ test/crossStreet.test.js
@@ -1,8 +1,7 @@
 var crossStreets = require('../lib/verifymatch.js').crossStreets;
 var tape = require('tape');
 
-tape('Find intersection of two crossing line features', function(assert) {
-    // given the geometries of two line features, return a feature matching the intersection
+tape('Find intersection of two crossing LineFeatures', function(assert) {
     var feature1 = {
         type: 'Feature',
         id:1,
@@ -30,11 +29,43 @@ tape('Find intersection of two crossing line features', function(assert) {
 
     var features = [feature1, feature2];
     var result = crossStreets(features);
-    // console.log('test result:', JSON.stringify(result, null, 2));
 
     assert.equals(result[0]['properties']['carmen:text'], 'fake street & main street');
     assert.deepEquals(result[0]['geometry']['coordinates'], [5, 5]);
+    assert.end();
+});
 
+tape('Find intersection of two touching LineFeatures', function(assert) {
+    var feature1 = {
+        type: 'Feature',
+        id:1,
+        properties: {
+            'carmen:text': 'fake street',
+            'carmen:center': [5,0],
+        },
+        geometry: {
+            type: 'LineString',
+            coordinates: [[5,0],[5,10]]
+        }
+    };
+    var feature2 = {
+        type: 'Feature',
+        id:2,
+        properties: {
+            'carmen:text': 'main street',
+            'carmen:center': [5,2],
+        },
+        geometry: {
+            type: 'LineString',
+            coordinates: [[5,2],[10,2]]
+        }
+    };
+
+    var features = [feature1, feature2];
+    var result = crossStreets(features);
+
+    assert.equals(result[0]['properties']['carmen:text'], 'fake street & main street');
+    assert.deepEquals(result[0]['geometry']['coordinates'], [5, 2]);
     assert.end();
 });
 
