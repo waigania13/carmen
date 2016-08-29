4fc788bfa0429512f9e895f656d3fce37c89e2ac
arunasank <aruna@mapbox.com>
2016-08-29 06:26:01 +0530

    new test for scoreAbove

---
+++ test/geocode-unit.scoreAbove.test.js
@@ -0,0 +1,57 @@
+// Tests Eiffel Tower (landmark) vs Eiffel Tower (dry cleaners)
+// Eiffel Tower should win via scoreAbove bonus.
+
+var tape = require('tape');
+var Carmen = require('..');
+var index = require('../lib/index');
+var context = require('../lib/context');
+var mem = require('../lib/api-mem');
+var queue = require('d3-queue').queue;
+var addFeature = require('../lib/util/addfeature');
+
+(function() {
+    var conf = {
+        poi: new mem(null, function() {})
+    };
+    var c = new Carmen(conf);
+    var tajMahalVarious = ['Taj Mahal Dry Cleaners', 'Taj Mahal Photos', 'Taj Mahal restaurant', 'Taj Mahal cafe']
+    
+    tape('index insignificant Taj Mahal pois (noise)', function(t) {
+        var q = queue(1);
+        for (var i = 1; i < 5; i++) q.defer(function(i, done) {
+            addFeature(conf.poi, {
+                id:i,
+                properties: {
+                    'carmen:score':i,
+                    'carmen:text': tajMahalVarious[i-1],
+                    'carmen:zxy':['6/32/32'],
+                    'carmen:center':[i,0]
+                }
+            }, done);
+        }, i);
+        q.awaitAll(t.end);
+    });
+    
+    tape('index Taj Mahal (landmark)', function(t) {
+        addFeature(conf.poi, {
+            id:5,
+            properties: {
+                'carmen:score': 1e9,
+                'carmen:text': 'Taj Mahal',
+                'carmen:zxy': ['6/33/32'],
+                'carmen:center': [0,0]
+            }
+        }, t.end);
+    });
+    tape('query', function(t) {
+        c.geocode('Taj Mahal', { scoreAbove:1 }, function(err, res) {
+            t.end();
+        });
+    });
+})();
+
+tape('teardown', function(assert) {
+    context.getTile.cache.reset();
+    assert.end();
+});
+
