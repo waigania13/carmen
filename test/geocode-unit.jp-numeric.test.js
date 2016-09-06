ffeba036780fce9b869e34cc703f974b9e9c49c0
Matt Ficke <matt@mattficke.com>
2016-09-06 18:07:23 -0400

    fix test geom

--- test/geocode-unit.jp-numeric.test.js
+++ test/geocode-unit.jp-numeric.test.js
@@ -68,7 +68,7 @@ tape('index address 1', function(t) {
         },
         geometry: {
             type: 'MultiPoint',
-            coordinates: [[0,0],[0,0],[0,0]]
+            coordinates: [[0,0],[0,0]]
         }
     };
     addFeature(conf.address, address, t.end);
