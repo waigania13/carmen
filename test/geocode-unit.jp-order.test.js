4c9efc2e1b2167b9d12a61e7a3c4025ed385cb34
Matt Ficke <matt@mattficke.com>
2016-09-20 12:40:25 -0400

    fix linter errors

--- test/geocode-unit.jp-order.test.js
+++ test/geocode-unit.jp-order.test.js
@@ -1,13 +1,8 @@
 var tape = require('tape');
 var Carmen = require('..');
-var index = require('../lib/index');
 var context = require('../lib/context');
 var mem = require('../lib/api-mem');
-var queue = require('d3-queue').queue;
 var addFeature = require('../lib/util/addfeature');
-var unidecode = require('unidecode-cxx');
-var token = require('../lib/util/token');
-var termops = require('../lib/util/termops');
 
 var conf = {
     country: new mem(null, function() {}),
