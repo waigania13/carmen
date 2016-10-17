4c82cc137eb1107e0ff7ec4c58b007891f24648c
Matt Ficke <matt@mattficke.com>
2016-10-17 15:24:17 -0400

    more log scale tests

--- test/geocode-unit.scoresort.test.js
+++ test/geocode-unit.scoresort.test.js
@@ -6,12 +6,15 @@ var context = require('../lib/context');
 var mem = require('../lib/api-mem');
 var addFeature = require('../lib/util/addfeature');
 var queue = require('d3-queue').queue;
+var termops = require('../lib/util/termops');
 
 (function() {
 
     var conf = {
         region: new mem(null, function() {}),
-        place: new mem(null, function() {})
+        place: new mem(null, function() {}),
+        lamplace: new mem(null, function() {}),
+        namplace: new mem(null, function() {})
     };
     var c = new Carmen(conf);
     // very high max score in region index
@@ -21,7 +24,7 @@ var queue = require('d3-queue').queue;
             properties: {
                 'carmen:text':'bigtown',
                 'carmen:zxy':['6/32/32'],
-                'carmen:score':100000,
+                'carmen:score':160000,
                 'carmen:center':[0,0]
             }
         }, t.end);
@@ -30,7 +33,24 @@ var queue = require('d3-queue').queue;
     // Many low-scored features in region index
     tape('index region (low score)', function(t) {
         var q = queue(1);
-        for (var i =2; i < 22; i++) q.defer(function(i, done) {
+        for (var i =2; i < 25; i++) q.defer(function(i, done) {
+            addFeature(conf.region, {
+                id:i,
+                properties: {
+                    'carmen:text':'smallville' + i,
+                    'carmen:zxy':['6/32/32'],
+                    'carmen:score':1,
+                    'carmen:center':[0,0]
+                }
+            }, done);
+        }, i);
+        q.awaitAll(t.end);
+    });
+
+    // Many medium-scored features in region index
+    tape('index region (medium score)', function(t) {
+        var q = queue(1);
+        for (var i =25; i < 50; i++) q.defer(function(i, done) {
             addFeature(conf.region, {
                 id:i,
                 properties: {
@@ -57,11 +77,52 @@ var queue = require('d3-queue').queue;
         }, t.end);
     });
 
+    tape('index lamplace (high score)', function(t) {
+        addFeature(conf.namplace, {
+            id:1,
+            properties: {
+                'carmen:text':'smallville1',
+                'carmen:zxy':['6/32/32'],
+                'carmen:score':36500,
+                'carmen:center':[0,0]
+            }
+        }, t.end);
+    });
+
+    // Many medium-scored features in region index
+    tape('index lamplace (medium score)', function(t) {
+        var q = queue(1);
+        for (var i =2; i < 25; i++) q.defer(function(i, done) {
+            addFeature(conf.region, {
+                id:i,
+                properties: {
+                    'carmen:text':'smallville' + i,
+                    'carmen:zxy':['6/32/32'],
+                    'carmen:score':6000,
+                    'carmen:center':[0,0]
+                }
+            }, done);
+        }, i);
+        q.awaitAll(t.end);
+    });
+
+    tape('index namplace (high score)', function(t) {
+        addFeature(conf.namplace, {
+            id:1,
+            properties: {
+                'carmen:text':'smallville1',
+                'carmen:zxy':['6/32/32'],
+                'carmen:score':16000,
+                'carmen:center':[0,0]
+            }
+        }, t.end);
+    });
+
     // High-scored feature wins over low-scored features in index with high max score
     tape('high score beats low score + high scorefactor', function(t) {
         c.geocode('smallville', null, function(err, res) {
             t.ifError(err);
-            t.equal(res.features[0].id, "place.1", "Place (high score) is first result")
+            t.equal(res.features[0].id, "namplace.1", "Place (high score) is first result")
             t.end();
         });
     });
@@ -71,3 +132,4 @@ var queue = require('d3-queue').queue;
     });
 
 })();
+
