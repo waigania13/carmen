e0aee7a6eb43bf9fcf59b2e912c35bb54687df1a
Andrew Pendleton <andrew@mapbox.com>
2016-10-19 00:25:36 -0400

    Make sure the translation autocomplete flag functionality works with both the in-memory and lazy caches, now that their codepaths are diverging

--- test/geocode-unit.translation-noauto.test.js
+++ test/geocode-unit.translation-noauto.test.js
@@ -2,11 +2,12 @@
 
 var tape = require('tape');
 var Carmen = require('..');
+var Cache = require('../lib/util/cxxcache');
 var context = require('../lib/context');
 var mem = require('../lib/api-mem');
 var addFeature = require('../lib/util/addfeature');
 
-(function() {
+var runTests = function(mode) {
     var conf = { region: new mem(null, function() {}) };
     var c = new Carmen(conf);
     tape('index first region', function(t) {
@@ -30,6 +31,26 @@ var addFeature = require('../lib/util/addfeature');
             }
         }, t.end);
     });
+
+    if (mode == "lazy") {
+        // on the second run through the tests, force carmen-cache to use lazy
+        // instead of in-memory caching
+        tape('reload cache', function(t) {
+            var oldCache = c.byidx[0]._geocoder;
+            var newCache = new Cache(oldCache.id);
+
+            ['freq', 'grid'].forEach(function(type) {
+                var shards = oldCache.list(type);
+                shards.forEach(function(shard) {
+                    newCache.loadSync(oldCache.pack(type, shard), type, shard);
+                });
+            });
+
+            c.byidx[0]._geocoder = newCache;
+            t.end();
+        });
+    }
+
     tape('de', function(t) {
         c.geocode('de', {}, function(err, res) {
             t.ifError(err);
@@ -75,7 +96,11 @@ var addFeature = require('../lib/util/addfeature');
             t.end();
         });
     });
-})();
+
+    if (mode == "memory_cache") runTests("lazy");
+};
+
+runTests("memory_cache");
 
 tape('teardown', function(assert) {
     context.getTile.cache.reset();
