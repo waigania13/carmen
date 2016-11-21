892ff479966eb1c36e107a3e4f7991eceda9ba15
Ansis Brammanis <brammanis@gmail.com>
2016-11-21 11:54:04 -0800

    add Phrasematch and PhrasematchResult types
    The main goal is to make these types more explicit and rigid so it's
    easier to understand what the objects used everywhere are.
    
    This also starts splits (some) source-level properties like `loadall`
    and `getter` out of Phrasematch to make it clearer that they are
    source-level.

--- test/spatialmatch.stackable.test.js
+++ test/spatialmatch.stackable.test.js
@@ -1,15 +1,18 @@
 var stackable = require('../lib/spatialmatch.js').stackable;
+var phrasematch = require('../lib/phrasematch');
+var Phrasematch = phrasematch.Phrasematch;
+var PhrasematchResult = phrasematch.PhrasematchResult;
 var test = require('tape');
 
 test('stackable simple', function(assert) {
-    var a1 = { text:"a1", idx:0, zoom:0, mask:parseInt('10',2), weight:0.5 };
-    var b1 = { text:"b1", idx:1, zoom:1, mask:parseInt('1',2), weight:0.5 };
-    var b2 = { text:"b2", idx:1, zoom:1, mask:parseInt('1',2), weight:0.5 };
+    var a1 = new Phrasematch(['a1'], 0.5, parseInt('10', 2), null, null, 0, null, 0);
+    var b1 = new Phrasematch(['b1'], 0.5, parseInt('1', 2), null, null, 1, null, 1);
+    var b2 = new Phrasematch(['b2'], 0.5, parseInt('1', 2), null, null, 1, null, 1);
     var debug = stackable([
-        [ a1 ],
-        [ b1, b2 ],
+        new PhrasematchResult([a1], null, null, { idx: 0, bmask: {}, ndx: 0 }),
+        new PhrasematchResult([b1, b2], null, null, { idx: 1, bmask: {}, ndx: 1 })
     ]).map(function(stack) {
-        return stack.map(function(s) { return s.text });
+        return stack.map(function(s) { return s.subquery.join(' '); });
     });
     assert.deepEqual(debug, [
         [ 'a1', 'b1' ],
@@ -19,15 +22,15 @@ test('stackable simple', function(assert) {
 });
 
 test('stackable nmask', function(assert) {
-    var a1 = { text:"a1", idx:0, zoom:1, mask:parseInt('100',2), nmask:parseInt('1',2), weight:0.33 };
-    var b1 = { text:"b1", idx:1, zoom:1, mask:parseInt('10',2), nmask:parseInt('10',2), weight:0.33 };
-    var c1 = { text:"c1", idx:2, zoom:1, mask:parseInt('1',2), nmask:parseInt('10',2), weight:0.33 };
+    var a1 = new Phrasematch(['a1'], 0.33, parseInt('100', 2), null, null, 0, null, 1);
+    var b1 = new Phrasematch(['b1'], 0.33, parseInt('10', 2), null, null, 1, null, 1);
+    var c1 = new Phrasematch(['c1'], 0.33, parseInt('1', 2), null, null, 2, null, 1);
     var debug = stackable([
-        [ a1 ],
-        [ b1 ],
-        [ c1 ],
+        new PhrasematchResult([a1], null, null, { idx: 0, bmask: {}, ndx: 0 }),
+        new PhrasematchResult([b1], null, null, { idx: 1, bmask: {}, ndx: 1 }),
+        new PhrasematchResult([c1], null, null, { idx: 2, bmask: {}, ndx: 1 })
     ]).map(function(stack) {
-        return stack.map(function(s) { return s.text });
+        return stack.map(function(s) { return s.subquery.join(' ')});
     });
     assert.deepEqual(debug, [
         [ 'b1', 'a1' ],
@@ -37,13 +40,13 @@ test('stackable nmask', function(assert) {
 });
 
 test('stackable bmask', function(assert) {
-    var a1 = { text:"a1", idx:0, zoom:1, mask:parseInt('100',2), bmask:[0,1], weight:0.66 };
-    var b1 = { text:"b1", idx:1, zoom:1, mask:parseInt('10',2), bmask:[1,0], weight:0.66 };
+    var a1 = new Phrasematch(['a1'], 0.66, parseInt('100', 2), null, null, 0, null, 1);
+    var b1 = new Phrasematch(['b1'], 0.66, parseInt('10', 2), null, null, 1, null, 1);
     var debug = stackable([
-        [ a1 ],
-        [ b1 ],
+        new PhrasematchResult([a1], null, null, { idx: 0, bmask: [0, 1], ndx: 0 }),
+        new PhrasematchResult([b1], null, null, { idx: 1, bmask: [1, 0], ndx: 1 })
     ]).map(function(stack) {
-        return stack.map(function(s) { return s.text });
+        return stack.map(function(s) { return s.subquery.join(' '); });
     });
     assert.deepEqual(debug, [
         [ 'a1' ],
@@ -53,18 +56,18 @@ test('stackable bmask', function(assert) {
 });
 
 test('stackable complex', function(assert) {
-    var a1 = { text:"a1", idx:0, zoom:0, mask:parseInt('10',2), weight:0.33 };
-    var a2 = { text:"a2", idx:0, zoom:0, mask:parseInt('110',2), weight:0.66 };
-    var b1 = { text:"b1", idx:1, zoom:1, mask:parseInt('1',2), weight:0.33 };
-    var b2 = { text:"b2", idx:1, zoom:1, mask:parseInt('100',2), weight:0.33 };
-    var c1 = { text:"c1", idx:1, zoom:1, mask:parseInt('1',2), weight:0.33 };
-    var c2 = { text:"c2", idx:1, zoom:1, mask:parseInt('100',2), weight:0.33 };
+    var a1 = new Phrasematch(['a1'], 0.33, parseInt('10', 2), null, null, 0, null, 0);
+    var a2 = new Phrasematch(['a2'], 0.66, parseInt('110', 2), null, null, 0, null, 0);
+    var b1 = new Phrasematch(['b1'], 0.33, parseInt('1', 2), null, null, 1, null, 1);
+    var b2 = new Phrasematch(['b2'], 0.33, parseInt('100', 2), null, null, 1, null, 1);
+    var c1 = new Phrasematch(['c1'], 0.33, parseInt('1', 2), null, null, 1, null, 1);
+    var c2 = new Phrasematch(['c2'], 0.33, parseInt('100', 2), null, null, 1, null, 1);
     var debug = stackable([
-        [ a1, a2 ],
-        [ b1, b2 ],
-        [ c1, c2 ],
+        new PhrasematchResult([a1, a2], null, null, { idx: 0, bmask: [], ndx: 0 }),
+        new PhrasematchResult([b1, b2], null, null, { idx: 1, bmask: [], ndx: 1 }),
+        new PhrasematchResult([c1, c2], null, null, { idx: 1, bmask: [], ndx: 2 }),
     ]).map(function(stack) {
-        return stack.relev.toFixed(2) + ' - ' + stack.map(function(s) { return s.text }).join(', ');
+        return stack.relev.toFixed(2) + ' - ' + stack.map(function(s) { return s.subquery.join(' ')}).join(', ');
     });
     assert.deepEqual(debug, [
         '0.99 - a2, c1',
@@ -81,21 +84,21 @@ test('stackable complex', function(assert) {
 });
 
 test('stackable direction change', function(assert) {
-    var a1 = { text:"a1", idx:0, zoom:0, mask:parseInt('0001',2), weight:0.25 };
-    var a2 = { text:"a2", idx:0, zoom:0, mask:parseInt('1000',2), weight:0.25 };
-    var b1 = { text:"b1", idx:1, zoom:1, mask:parseInt('0010',2), weight:0.25 };
-    var b2 = { text:"b2", idx:1, zoom:1, mask:parseInt('0100',2), weight:0.25 };
-    var c1 = { text:"c1", idx:2, zoom:2, mask:parseInt('0100',2), weight:0.25 };
-    var c2 = { text:"c2", idx:2, zoom:2, mask:parseInt('0010',2), weight:0.25 };
-    var d1 = { text:"d1", idx:3, zoom:3, mask:parseInt('1000',2), weight:0.25 };
-    var d2 = { text:"d2", idx:3, zoom:3, mask:parseInt('0001',2), weight:0.25 };
+    var a1 = new Phrasematch(['a1'], 0.25, parseInt('0001', 2), null, null, 0, null, 0);
+    var a2 = new Phrasematch(['a2'], 0.25, parseInt('1000', 2), null, null, 0, null, 0);
+    var b1 = new Phrasematch(['b1'], 0.25, parseInt('0010', 2), null, null, 1, null, 1);
+    var b2 = new Phrasematch(['b2'], 0.25, parseInt('0100', 2), null, null, 1, null, 1);
+    var c1 = new Phrasematch(['c1'], 0.25, parseInt('0100', 2), null, null, 2, null, 2);
+    var c2 = new Phrasematch(['c2'], 0.25, parseInt('0010', 2), null, null, 2, null, 2);
+    var d1 = new Phrasematch(['d1'], 0.25, parseInt('1000', 2), null, null, 3, null, 3);
+    var d2 = new Phrasematch(['d2'], 0.25, parseInt('0001', 2), null, null, 3, null, 4);
     var debug = stackable([
-        [ a1, a2 ],
-        [ b1, b2 ],
-        [ c1, c2 ],
-        [ d1, d2 ]
+        new PhrasematchResult([a1, a2], null, null, { idx: 0, bmask: [], ndx: 0 }),
+        new PhrasematchResult([b1, b2], null, null, { idx: 1, bmask: [], ndx: 1 }),
+        new PhrasematchResult([c1, c2], null, null, { idx: 2, bmask: [], ndx: 2 }),
+        new PhrasematchResult([d1, d2], null, null, { idx: 3, bmask: [], ndx: 3 }),
     ]).map(function(stack) {
-        return stack.map(function(s) { return s.text });
+        return stack.map(function(s) { return s.subquery.join(' ')});
     });
     assert.deepEqual(debug, [
         [ 'a2', 'b2', 'c2', 'd2' ],
@@ -149,14 +152,9 @@ test('stackable bench', function(assert) {
                 for (var o = 0; o < matchingTerms; o++) {
                     mask = mask | (1 << (offset + o));
                 }
-                phraseMatches[i] = phraseMatches[i] || [];
-                phraseMatches[i].push({
-                    text: t + '-' + i,
-                    idx: i,
-                    zoom: 0,
-                    mask: mask,
-                    weight: matchingTerms/termCount
-                });
+                phraseMatches[i] = phraseMatches[i] || new PhrasematchResult([], null, null, { idx: i, bmask: [], ndx: i });
+                var weight = matchingTerms / termCount;
+                phraseMatches[i].phrasematches.push(new Phrasematch([t + '-' + i], weight, mask, null, null, i, null, 0));
             }
         }
         var start = +new Date;
