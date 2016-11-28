7c4849b648073fafe79490779208dfa1bbb90857
Young Hahn <young@mapbox.com>
2016-11-28 02:53:27 -0500

    Allow any carmen:text language fields to have squishy affinity

--- test/geocode-unit.promote-language.test.js
+++ test/geocode-unit.promote-language.test.js
@@ -1,12 +1,13 @@
-// Tests New York (place), New York (region), USA (country)
-// identically-named features should reverse the gappy penalty and
-// instead prioritize the highest-index feature
-
 var tape = require('tape');
 var Carmen = require('..');
 var context = require('../lib/context');
 var mem = require('../lib/api-mem');
 var addFeature = require('../lib/util/addfeature');
+
+// Tests New York (place), New York (region), USA (country)
+// identically-named features should reverse the gappy penalty and
+// instead prioritize the highest-index feature
+(function() {
 
 var conf = {
     country: new mem({ maxzoom: 6 }, function() {}),
@@ -102,4 +103,99 @@ tape('teardown', function(assert) {
     context.getTile.cache.reset();
     assert.end();
 });
+
+})();
+
+// Simulate a case where carmen:text has a discrepancy but carmen:text_en
+// allows a text match to occur.
+(function() {
+
+var conf = {
+    country: new mem({ maxzoom: 6 }, function() {}),
+    region: new mem({ maxzoom: 6 }, function() {}),
+    place: new mem({ maxzoom: 6, geocoder_inherit_score: true }, function() {})
+};
+
+var c = new Carmen(conf);
+
+tape('index country', function(t) {
+    addFeature(conf.country, {
+        id: 1,
+        properties: {
+            'carmen:center': [0,0],
+            'carmen:score': 1,
+            'carmen:text':'saudi arabia'
+        },
+        geometry: {
+            type: 'Polygon',
+            coordinates: [[
+                [-20,-20],
+                [-20,20],
+                [20,20],
+                [20,-20],
+                [-20,-20],
+            ]]
+        }
+    }, t.end);
+});
+
+tape('index region', function(t) {
+    addFeature(conf.region, {
+        id: 1,
+        properties: {
+            'carmen:center': [0,0],
+            'carmen:score': 1,
+            'carmen:text': 'مكة',
+            'carmen:text_en':'Makkah'
+        },
+        geometry: {
+            type: 'Polygon',
+            coordinates: [[
+                [-20,-20],
+                [-20,20],
+                [20,20],
+                [20,-20],
+                [-20,-20],
+            ]]
+        }
+    }, t.end);
+});
+
+tape('index place', function(t) {
+    addFeature(conf.place, {
+        id: 1,
+        properties: {
+            'carmen:center': [0,0],
+            'carmen:score': 1,
+            'carmen:text':'Makkah Al Mukarramah',
+            'carmen:text_en':'Makkah'
+        },
+        geometry: {
+            type: 'Polygon',
+            coordinates: [[
+                [-20,-20],
+                [-20,20],
+                [20,20],
+                [20,-20],
+                [-20,-20],
+            ]]
+        }
+    }, t.end);
+});
+
+tape('find makkah', function(t) {
+    c.geocode('makkah', {}, function(err, res) {
+        t.equal(res.features[0].id, 'place.1');
+        t.equal(res.features[0].relevance, 0.99);
+        t.end();
+    });
+});
+
+tape('teardown', function(assert) {
+    context.getTile.cache.reset();
+    assert.end();
+});
+
+})();
+
 
