e5ec95c9b743ebe1a5855c563071d7578ac3aa4b
Young Hahn <young@mapbox.com>
2016-12-03 18:10:34 -0500

    Fix bug where a more specific multitype promotion could previously be muscled out by an intermediate type.

--- test/context.stackFeatures.test.js
+++ test/context.stackFeatures.test.js
@@ -2,11 +2,17 @@ var context = require('../lib/context');
 var tape = require('tape');
 
 tape('context.stackFeatures noop', function(assert) {
-    assert.deepEqual(context.stackFeatures([], {}), [], '0 features => []');
+    assert.deepEqual(context.stackFeatures({}, [], {}), [], '0 features => []');
     assert.end();
 });
 
 tape('context.stackFeatures simple', function(assert) {
+    var geocoderStub = {
+        indexes: {
+            country: { type:'country' },
+            region: { type:'region' }
+        }
+    };
     var loaded = [{
         type: 'Feature',
         properties: {
@@ -20,11 +26,16 @@ tape('context.stackFeatures simple', function(assert) {
             'carmen:extid': 'region.1'
         }
     }];
-    assert.deepEqual(context.stackFeatures(loaded.slice(0), {}), [loaded[1], loaded[0]], '2 features stacked');
+    assert.deepEqual(context.stackFeatures(geocoderStub, loaded.slice(0), {}), [loaded[1], loaded[0]], '2 features stacked');
     assert.end();
 });
 
 tape('context.stackFeatures type bump', function(assert) {
+    var geocoderStub = {
+        indexes: {
+            country: { type:'country' },
+        }
+    };
     var loaded = [{
         type: 'Feature',
         properties: {
@@ -38,11 +49,17 @@ tape('context.stackFeatures type bump', function(assert) {
             'carmen:extid': 'country.2'
         }
     }];
-    assert.deepEqual(context.stackFeatures(loaded.slice(0), {}), [loaded[0]], '1 feature stacked, 1 bumped');
+    assert.deepEqual(context.stackFeatures(geocoderStub, loaded.slice(0), {}), [loaded[0]], '1 feature stacked, 1 bumped');
     assert.end();
 });
 
 tape('context.stackFeatures conflict', function(assert) {
+    var geocoderStub = {
+        indexes: {
+            place: { type:'place' },
+            address: { type:'address' }
+        }
+    };
     var loaded = [{
         type: 'Feature',
         properties: {
@@ -63,11 +80,17 @@ tape('context.stackFeatures conflict', function(assert) {
             'carmen:extid': 'poi.1'
         }
     }];
-    assert.deepEqual(context.stackFeatures(loaded.slice(0), {}), [loaded[1], loaded[0]], '2 features stacked, 1 bumped');
+    assert.deepEqual(context.stackFeatures(geocoderStub, loaded.slice(0), {}), [loaded[1], loaded[0]], '2 features stacked, 1 bumped');
     assert.end();
 });
 
 tape('context.stackFeatures conflict, dist tiebreak', function(assert) {
+    var geocoderStub = {
+        indexes: {
+            place: { type:'place' },
+            address: { type:'address' }
+        }
+    };
     var loaded = [{
         type: 'Feature',
         properties: {
@@ -90,11 +113,17 @@ tape('context.stackFeatures conflict, dist tiebreak', function(assert) {
             'carmen:vtquerydist': 1
         }
     }];
-    assert.deepEqual(context.stackFeatures(loaded.slice(0), {}), [loaded[2], loaded[0]], '2 features stacked, 1 bumped, conflict priorities nearest feature');
+    assert.deepEqual(context.stackFeatures(geocoderStub, loaded.slice(0), {}), [loaded[2], loaded[0]], '2 features stacked, 1 bumped, conflict priorities nearest feature');
     assert.end();
 });
 
 tape('context.stackFeatures multitype', function(assert) {
+    var geocoderStub = {
+        indexes: {
+            region: { type:'region' },
+            place: { type:'place' }
+        }
+    };
     var loaded = [{
         type: 'Feature',
         properties: {
@@ -102,13 +131,20 @@ tape('context.stackFeatures multitype', function(assert) {
             'carmen:extid': 'region.1'
         }
     }];
-    var stacked = context.stackFeatures(loaded.slice(0), {});
+    var stacked = context.stackFeatures(geocoderStub, loaded.slice(0), {});
     assert.deepEqual(stacked, [loaded[0]], '1 feature stacked, promoted');
     assert.deepEqual(stacked[0].properties['carmen:extid'], 'place.1', 'alters extid');
     assert.end();
 });
 
 tape('context.stackFeatures multitype, gap', function(assert) {
+    var geocoderStub = {
+        indexes: {
+            region: { type:'region' },
+            place: { type:'place' },
+            poi: { type:'poi' }
+        }
+    };
     var loaded = [{
         type: 'Feature',
         properties: {
@@ -122,7 +158,7 @@ tape('context.stackFeatures multitype, gap', function(assert) {
             'carmen:extid': 'poi.1'
         }
     }];
-    var stacked = context.stackFeatures(loaded.slice(0), {});
+    var stacked = context.stackFeatures(geocoderStub, loaded.slice(0), {});
     assert.deepEqual(stacked, [loaded[1],loaded[0]], '2 features stacked, 1 promoted');
     assert.deepEqual(stacked[0].properties['carmen:extid'], 'poi.1');
     assert.deepEqual(stacked[1].properties['carmen:extid'], 'place.1');
@@ -130,6 +166,13 @@ tape('context.stackFeatures multitype, gap', function(assert) {
 });
 
 tape('context.stackFeatures multitype, nogap', function(assert) {
+    var geocoderStub = {
+        indexes: {
+            region: { type:'region' },
+            place: { type:'place' },
+            poi: { type:'poi' }
+        }
+    };
     var loaded = [{
         type: 'Feature',
         properties: {
@@ -149,7 +192,7 @@ tape('context.stackFeatures multitype, nogap', function(assert) {
             'carmen:extid': 'poi.1'
         }
     }];
-    var stacked = context.stackFeatures(loaded.slice(0), {});
+    var stacked = context.stackFeatures(geocoderStub, loaded.slice(0), {});
     assert.deepEqual(stacked, [loaded[2],loaded[0]], '2 features stacked, 1 promoted');
     assert.deepEqual(stacked[0].properties['carmen:extid'], 'poi.1');
     assert.deepEqual(stacked[1].properties['carmen:extid'], 'place.1');
---
+++ test/geocode-unit.multitype-leapfrog.test.js
@@ -0,0 +1,86 @@
+// Test multitype behavior when multitype spans across another existing index
+
+var tape = require('tape');
+var Carmen = require('..');
+var context = require('../lib/context');
+var mem = require('../lib/api-mem');
+var addFeature = require('../lib/util/addfeature');
+
+var conf = {
+    region: new mem({maxzoom:6, geocoder_types:['region','place']}, function() {}),
+    district: new mem({maxzoom:6}, function() {}),
+    place: new mem({maxzoom:6}, function() {}),
+};
+var c = new Carmen(conf);
+
+tape('index region', function(t) {
+    addFeature(conf.region, {
+        id:1,
+        geometry: {
+            type: 'Polygon',
+            coordinates: [[
+                [-40,-40],
+                [-40,40],
+                [40,40],
+                [40,-40],
+                [-40,-40]
+            ]]
+        },
+        properties: {
+            'carmen:types': ['region', 'place'],
+            'carmen:text': 'capital',
+            'carmen:center': [0,0]
+        }
+    }, t.end);
+});
+
+tape('index district', function(t) {
+    addFeature(conf.district, {
+        id:1,
+        geometry: {
+            type: 'Polygon',
+            coordinates: [[
+                [-40,-40],
+                [-40,40],
+                [40,40],
+                [40,-40],
+                [-40,-40]
+            ]]
+        },
+        properties: {
+            'carmen:text': 'district',
+            'carmen:center': [0,0]
+        }
+    }, t.end);
+});
+
+tape('multitype reverse', function(assert) {
+    assert.comment('query:  0,0');
+    assert.comment('result: capital');
+    assert.comment('note:   shifted reverse');
+    c.geocode('0,0', {}, function(err, res) {
+        assert.ifError(err);
+        assert.deepEqual(res.features[0].place_name, 'capital');
+        assert.deepEqual(res.features[0].id, 'place.1');
+        assert.deepEqual(res.features[0].context, undefined);
+        assert.end();
+    });
+});
+
+tape('multitype forward, q=capital', function(assert) {
+    assert.comment('query:  capital');
+    assert.comment('result: capital');
+    assert.comment('note:   shifted forward');
+    c.geocode('capital', {}, function(err, res) {
+        assert.ifError(err);
+        assert.deepEqual(res.features[0].place_name, 'capital');
+        assert.deepEqual(res.features[0].id, 'place.1');
+        assert.deepEqual(res.features[0].context, undefined);
+        assert.end();
+    });
+});
+tape('teardown', function(assert) {
+    context.getTile.cache.reset();
+    assert.end();
+});
+
