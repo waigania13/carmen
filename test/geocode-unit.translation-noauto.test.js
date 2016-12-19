ffef78138e26622c9d395854f1943398fe2703c6
Andrew Pendleton <andrew@mapbox.com>
2016-12-19 17:24:21 -0500

    Change the way pack and load work in autocomplete test

--- test/geocode-unit.translation-noauto.test.js
+++ test/geocode-unit.translation-noauto.test.js
@@ -6,6 +6,7 @@ var Cache = require('../lib/util/cxxcache');
 var context = require('../lib/context');
 var mem = require('../lib/api-mem');
 var addFeature = require('../lib/util/addfeature');
+var tmp = require('tmp');
 
 var runTests = function(mode) {
     var conf = { region: new mem(null, function() {}) };
@@ -40,10 +41,10 @@ var runTests = function(mode) {
             var newCache = new Cache(oldCache.id);
 
             ['freq', 'grid'].forEach(function(type) {
-                var shards = oldCache.list(type);
-                shards.forEach(function(shard) {
-                    newCache.loadSync(oldCache.pack(type, shard), type, shard);
-                });
+                var rocksdb = tmp.tmpNameSync();
+
+                oldCache.pack(rocksdb, type);
+                newCache.loadSync(rocksdb, type);
             });
 
             c.byidx[0]._geocoder = newCache;
