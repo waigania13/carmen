776d57253047203250598e23a51244d214ad54f4
Kai <kai@mapbox.com>
2017-01-06 16:51:22 -0800

    dedupe combined results

--- test/geocode-unit.combine.test.js
+++ test/geocode-unit.combine.test.js
@@ -36,12 +36,12 @@ tape('index Atuan - country', function(t) {
     };
     addFeature(confA.country, countryAtuanA, t.end);
 });
-tape('index Atuan Street - address', function(t) {
+tape('index Atuan Ring Road - address', function(t) {
     var addressAtuanA = {
         id:2,
         properties: {
-            'carmen:score': 1500,
-            'carmen:text':'Atuan Street',
+            'carmen:score': 200,
+            'carmen:text':'Atuan Ring Road',
             'carmen:zxy':['6/32/32'],
             'carmen:center':[158,-8]
         }
@@ -81,7 +81,7 @@ tape('index Atuan Ring Road - address', function(t) {
             'carmen:score': 200,
             'carmen:text':'Atuan Ring Road',
             'carmen:zxy':['6/32/32'],
-            'carmen:center':[151,-11]
+            'carmen:center':[158,-8]
         }
     };
     addFeature(confB.address, addressAtuanRingRoadB, t.end);
@@ -124,6 +124,16 @@ tape('sort shards', function(t) {
     // geocode for atuan
     var q = queue(2);
     var results = [];
+    var defaultOptions = { 
+        stats: false,
+        debug: false,
+        allow_dupes: false,
+        indexes: false,
+        autocomplete: true,
+        bbox: false,
+        limit: 5,
+        allowed_idx: { '0': true, '1': true } 
+    }
 
     // binds `this` context to carmenA so it survives deferring
     // q.defer(carmenA.geocode.bind(carmenA), 'Atuan', null);
@@ -146,11 +156,19 @@ tape('sort shards', function(t) {
     q.awaitAll(function(err, items) {
         if (err) throw err;
         // pass results to combineResults
-        var combinedResults = combineResults(items);
+        var combinedResults = combineResults(items, defaultOptions);
         // get back new, tidy featureCollection
-        console.log('combined results:', JSON.stringify(combinedResults, null, 2));
+        var places = [];
+        for (var i = 0; i < combinedResults.length; i++) {
+            places.push(combinedResults[i].place_name + ' r:' + combinedResults[i].relevance + ' s:' + combinedResults[i].score);
+        }
+        console.log('places:', places);
         t.end();
     });
+
+    // things to test: 
+    // - correct number of features (deduped, but already over limit 5?)
+    // - features sorted correctly (score check)
 
 })
 
