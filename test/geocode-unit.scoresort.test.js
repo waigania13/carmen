dac44052c4c01a58093ea30e9aeb77088b6fafb6
Matt Ficke <matt@mattficke.com>
2017-01-24 11:42:27 -0500

    Log scale (#516)
    * add score sorting unit test
    
    * log scale score
    
    * update test for log scale scoring
    
    * better log scaling
    
    * more log scale tests
    
    * use log scale util function
    
    * fix linter errors
    
    * Test smaller score spread [ci skip]
    
    * Allowed max score util
    
    * Enforce valid JSDoc (warn for now) [ci skip]
    
    * set `maxScore` as a property of the parent `geocoder` object [ci skip]
    
    * Keep both allowed and raw max scores
    
    * Scale large max scores to 7x median cap
    
    * Adjust score threshold in tests
    
    * Tax the rich
    
    * :knife: max score cap
    
    * :knife: :knife: :knife:
    
    * Let process.stdout finish before exiting
    
    * Don't need to explicitly set exit code
    
    * update fixture
    
    * update fixtures
    
    * better spatialmatch scoredist approximation
    
    * don't use log scale for scoredist
    
    * fix scoredist calc
    
    * 17.6.5-0
    
    * tweak scoredist apporximation
    
    * handle scoredist vals > index max score
    
    * Add a few more tests for logScaler to demonstrate how values distribute
    
    * explicitly handle negative scores (ghost features) when log scaling

---
+++ test/geocode-unit.scoresort.test.js
@@ -0,0 +1,148 @@
+// scoredist unit test
+
+var tape = require('tape');
+var Carmen = require('..');
+var context = require('../lib/context');
+var mem = require('../lib/api-mem');
+var addFeature = require('../lib/util/addfeature');
+var queue = require('d3-queue').queue;
+
+(function() {
+
+    var conf = {
+        region: new mem(null, function() {}),
+        place: new mem(null, function() {}),
+        lamplace: new mem(null, function() {}),
+        namplace: new mem(null, function() {}),
+        locality: new mem(null, function() {})
+    };
+    var c = new Carmen(conf);
+    // very high max score in region index
+    tape('index region (high score)', function(t) {
+        addFeature(conf.region, {
+            id:1,
+            properties: {
+                'carmen:text':'bigtown',
+                'carmen:zxy':['6/32/32'],
+                'carmen:score':160000,
+                'carmen:center':[0,0]
+            }
+        }, t.end);
+    });
+
+    // Many low-scored features in region index
+    tape('index region (low score)', function(t) {
+        var q = queue(1);
+        for (var i =2; i < 25; i++) q.defer(function(i, done) {
+            addFeature(conf.region, {
+                id:i,
+                properties: {
+                    'carmen:text':'smallville' + i,
+                    'carmen:zxy':['6/32/32'],
+                    'carmen:score':1,
+                    'carmen:center':[0,0]
+                }
+            }, done);
+        }, i);
+        q.awaitAll(t.end);
+    });
+
+    // Many medium-scored features in region index
+    tape('index region (medium score)', function(t) {
+        var q = queue(1);
+        for (var i =25; i < 50; i++) q.defer(function(i, done) {
+            addFeature(conf.region, {
+                id:i,
+                properties: {
+                    'carmen:text':'smallville' + i,
+                    'carmen:zxy':['6/32/32'],
+                    'carmen:score':3000,
+                    'carmen:center':[0,0]
+                }
+            }, done);
+        }, i);
+        q.awaitAll(t.end);
+    });
+
+    // Feature is scored higher than all but one region
+    tape('index place (high score)', function(t) {
+        addFeature(conf.place, {
+            id:1,
+            properties: {
+                'carmen:text':'smallville1',
+                'carmen:zxy':['6/32/32'],
+                'carmen:score':10000,
+                'carmen:center':[0,0]
+            }
+        }, t.end);
+    });
+
+    tape('index lamplace (high score)', function(t) {
+        addFeature(conf.lamplace, {
+            id:1,
+            properties: {
+                'carmen:text':'smallville1',
+                'carmen:zxy':['6/32/32'],
+                'carmen:score':36500,
+                'carmen:center':[0,0]
+            }
+        }, t.end);
+    });
+
+    // Many medium-scored features in region index
+    tape('index lamplace (medium score)', function(t) {
+        var q = queue(1);
+        for (var i =2; i < 25; i++) q.defer(function(i, done) {
+            addFeature(conf.lamplace, {
+                id:i,
+                properties: {
+                    'carmen:text':'smallville' + i,
+                    'carmen:zxy':['6/32/32'],
+                    'carmen:score':6000,
+                    'carmen:center':[0,0]
+                }
+            }, done);
+        }, i);
+        q.awaitAll(t.end);
+    });
+
+    tape('index namplace (high score)', function(t) {
+        addFeature(conf.namplace, {
+            id:1,
+            properties: {
+                'carmen:text':'smallville1',
+                'carmen:zxy':['6/32/32'],
+                'carmen:score':16000,
+                'carmen:center':[0,0]
+            }
+        }, t.end);
+    });
+
+    tape('index locality (low score)', function(t) {
+        addFeature(conf.locality, {
+            id:1,
+            properties: {
+                'carmen:text':'smallville1',
+                'carmen:zxy':['6/32/32'],
+                'carmen:score':1000,
+                'carmen:center':[0,0]
+            }
+        }, t.end);
+    });
+
+    // High-scored feature wins over low-scored features in index with high max score
+    tape('high score beats low score + high scorefactor', function(t) {
+        c.geocode('smallville', null, function(err, res) {
+            t.ifError(err);
+            t.equal(res.features[0].id, "lamplace.1", "Place (high score) is first result")
+            t.equal(res.features[1].id, "namplace.1", "Place (high score) is second result")
+            t.end();
+        });
+    });
+    tape('teardown', function(assert) {
+        context.getTile.cache.reset();
+        assert.end();
+    });
+
+})();
+
