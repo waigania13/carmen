246709751505b1666340a43b5589345faf954b08
Bob Lannon <blannon@gmail.com>
2017-01-27 14:59:16 -0500

    updated tests

--- test/termops.segmentWords.test.js
+++ test/termops.segmentWords.test.js
@@ -6,16 +6,13 @@ test('tokenizes Japanese strings containing CJK characters', function(assert) {
     assert.end();
 });
 
-test('edge cases - empty string', function(assert) {
+test('edge case - empty string', function(assert) {
     assert.deepEqual(termops.segmentWords(''), []);
     assert.end();
 });
 
-test('tokenize Japanese strings with numeric component', function(assert) {
-    assert.deepEqual(termops.segmentWords('岐阜県中津川市馬籠4571-1'),  ['岐阜県', '中津', '川市', '馬', '籠', '4571', '-', '1'], 'dashed number at end');
-    assert.deepEqual(termops.segmentWords('岐阜県中津川市4571-1馬籠'),  ['岐阜県', '中津', '川市', '4571','-','1','馬','籠'], 'dashed number in middle');
-    assert.deepEqual(termops.segmentWords('岐阜県中津川市4571馬籠'),    ['岐阜県', '中津', '川市', '4571', '馬','籠'], 'number in middle');
-    assert.deepEqual(termops.segmentWords('岐阜県中津川市4571馬籠123'), ['岐阜県', '中津', '川市', '4571', '馬','籠','123'], 'numbers in middle and at end');
-    assert.deepEqual(termops.segmentWords('岐阜県123中津川市4571馬籠'), ['123中津川市4571馬籠'], 'does not split strings that begin with numbers');
+test('edge case - short strings', function(assert) {
+    assert.deepEqual(termops.segmentWords('馬'), ['馬']);
+    assert.deepEqual(termops.segmentWords('馬籠'), ['馬', '籠']);
     assert.end();
 });
--- test/termops.tokenize.test.js
+++ test/termops.tokenize.test.js
@@ -25,7 +25,9 @@ test('tokenizes basic strings', function(assert) {
     assert.deepEqual(termops.tokenize('San José'), ['san', 'josé']);
     assert.deepEqual(termops.tokenize('Chamonix-Mont-Blanc'), ['chamonix','mont','blanc']);
     assert.deepEqual(termops.tokenize('Москва'), ['москва']);
-    assert.deepEqual(termops.tokenize('京都市'), ['京','都','市']);
+    assert.deepEqual(termops.tokenize('京都市'), ['京都市']);
+    assert.deepEqual(termops.tokenize('市都京'), ['市', '都', '京']);
+    assert.deepEqual(termops.tokenize('岐阜県中津'), ['岐阜県','中津']);
     assert.end();
 });
 test('tokenizes lonlat', function(assert) {
@@ -50,10 +52,10 @@ test('edge cases - empty string', function(assert) {
 });
 
 test('tokenize Japanese strings with numeric component', function(assert) {
-    assert.deepEqual(termops.tokenize('中津川市馬籠4571-1'), ['中','津','川','市','馬','籠','4571','-','1'], 'dashed number at end');
-    assert.deepEqual(termops.tokenize('中津川市4571-1馬籠'), ['中','津','川','市','4571','-','1','馬','籠'], 'dashed number in middle');
-    assert.deepEqual(termops.tokenize('中津川市4571馬籠'), ['中','津','川','市','4571','馬','籠'], 'number in middle');
-    assert.deepEqual(termops.tokenize('中津川市4571馬籠123'), ['中','津','川','市','4571','馬','籠','123'], 'numbers in middle and at end');
+    assert.deepEqual(termops.tokenize('岐阜県中津川市馬籠4571-1'),  ['岐阜県', '中津', '川市', '馬', '籠', '4571', '-', '1'], 'dashed number at end');
+    assert.deepEqual(termops.tokenize('岐阜県中津川市4571-1馬籠'),  ['岐阜県', '中津', '川市', '4571','-','1','馬','籠'], 'dashed number in middle');
+    assert.deepEqual(termops.tokenize('岐阜県中津川市4571馬籠'),    ['岐阜県', '中津', '川市', '4571', '馬','籠'], 'number in middle');
+    assert.deepEqual(termops.tokenize('岐阜県中津川市4571馬籠123'), ['岐阜県', '中津', '川市', '4571', '馬','籠','123'], 'numbers in middle and at end');
     assert.deepEqual(termops.tokenize('123中津川市4571馬籠'), ['123中津川市4571馬籠'], 'does not split strings that begin with numbers');
     assert.end();
 });
