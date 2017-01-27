19022637345c99e19896299121eef8d6a0819936
Dane Springmeyer <dane@mapbox.com>
2017-01-27 13:55:17 -0800

    actually exit non-zero if any benchmarks are broken

---
+++ test/run_all_benchmarks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+failures=0
+
+for file in bench/ph*.js; do
+    echo "Running $file"
+    node $file || echo "$file failed" && failures=$((failures+1));
+done
+
+if [[ ${failures} == 0 ]]; then
+    echo "Success: All benchmarks ran to completion without errors"
+    exit 0
+else
+    echo "Error: $failures benchmarks failed to run"
+    exit 1
+fi
