04816dea726fd1e9ee4df685facbadf7c883b9b4
Dane Springmeyer <dane@mapbox.com>
2017-01-27 15:53:36 -0800

    fix reporting of benchmark runner

--- test/run_all_benchmarks.sh
+++ test/run_all_benchmarks.sh
@@ -3,16 +3,24 @@
 set -euo pipefail
 
 failures=0
+success=0
 
 for file in bench/*.js; do
     echo "Running $file"
-    node $file || echo "$file failed" && failures=$((failures+1));
+    FAILED=0
+    node $file || FAILED=1
+    if [[ ${FAILED} != 0 ]]; then
+        echo "***** ERROR: $file failed to run *****"
+        failures=$((failures+1))
+    else
+        success=$((success+1))
+    fi
 done
 
 if [[ ${failures} == 0 ]]; then
-    echo "Success: All benchmarks ran to completion without errors"
+    echo "Success: All ${success} benchmarks ran to completion without errors"
     exit 0
 else
-    echo "Error: $failures benchmarks failed to run"
+    echo "Error: $failures benchmarks failed to run (${success} succeeded)"
     exit 1
 fi
