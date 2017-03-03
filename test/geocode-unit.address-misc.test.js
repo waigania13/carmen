36a96dd8d007bb628568b588dee5044443644f45
ingalls <nicholas.ingalls@gmail.com>
2017-03-03 17:04:34 -0500

    <3 ESLint

--- test/geocode-unit.address-misc.test.js
+++ test/geocode-unit.address-misc.test.js
@@ -4,7 +4,6 @@ var tape = require('tape');
 var Carmen = require('..');
 var context = require('../lib/context');
 var mem = require('../lib/api-mem');
-var queue = require('d3-queue').queue;
 var addFeature = require('../lib/util/addfeature'),
     queueFeature = addFeature.queueFeature,
     buildQueued = addFeature.buildQueued;
