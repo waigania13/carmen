ee598fc83a02fe89a34d5ad5d845383ad0969dee
ingalls <nick@mapbox.com>
2017-05-03 11:32:33 -0400

    Another set of ES6

--- test/dawgcache.test.js
+++ test/dawgcache.test.js
@@ -1,41 +1,41 @@
-var tape = require('tape');
-var zlib = require('zlib');
-var DawgCache = require('../lib/util/dawg');
+const tape = require('tape');
+const zlib = require('zlib');
+const DawgCache = require('../lib/util/dawg');
 
-tape('create', function(assert) {
-    var dict = new DawgCache();
-    assert.ok(dict, "dawg created")
-    assert.end();
+tape('create', function(t) {
+    const dict = new DawgCache();
+    t.ok(dict, "dawg created")
+    t.end();
 });
 
-tape('dump/load', function(assert) {
-    var dict = new DawgCache();
+tape('dump/load', function(t) {
+    const dict = new DawgCache();
     dict.setText("a1");
     dict.setText("a2");
     dict.setText("a3");
     dict.setText("a4");
 
     zlib.gzip(dict.dump(), function(err, zdata) {
-        assert.ifError(err);
-        assert.ok(zdata.length < 200e3, 'gzipped dictcache < 200k');
+        t.ifError(err);
+        t.ok(zdata.length < 200e3, 'gzipped dictcache < 200k');
         zlib.gunzip(zdata, function(err, data) {
-            assert.ifError(err);
+            t.ifError(err);
             var loaded = new DawgCache(data);
             for (var i = 1; i <= 4; i++) {
-                assert.equal(loaded.hasPhrase("a" + i, false), true, 'has a' + i);
+                t.equal(loaded.hasPhrase("a" + i, false), true, 'has a' + i);
             }
-            assert.equal(loaded.hasPhrase("a5", false), false, 'not a5');
+            t.equal(loaded.hasPhrase("a5", false), false, 'not a5');
 
-            assert.equal(loaded.hasPhrase("a", false), false, 'not a');
-            assert.equal(loaded.hasPhrase("a", true), true, 'has a as degen');
+            t.equal(loaded.hasPhrase("a", false), false, 'not a');
+            t.equal(loaded.hasPhrase("a", true), true, 'has a as degen');
 
-            assert.end();
+            t.end();
         });
     });
 });
 
-tape('invalid data', function(assert) {
-    var dict = new DawgCache();
-    assert.throws(function() { dict.setText(""); });
-    assert.end();
+tape('invalid data', function(t) {
+    const dict = new DawgCache();
+    t.throws(function() { dict.setText(""); });
+    t.end();
 });
--- test/feature.test.js
+++ test/feature.test.js
@@ -1,17 +1,17 @@
-var tape = require('tape');
-var feature = require('../lib/util/feature.js');
+const tape = require('tape');
+const feature = require('../lib/util/feature.js');
 
-tape('shard', function(assert) {
-    for (var level = 0; level < 7; level++) {
-        var shards = {};
-        for (var i = 0; i < Math.pow(2,20); i++) {
-            var shard = feature.shard(level, i);
+tape('shard', (t) => {
+    for (let level = 0; level < 7; level++) {
+        let shards = {};
+        for (let i = 0; i < Math.pow(2,20); i++) {
+            let shard = feature.shard(level, i);
             shards[shard] = shards[shard] || 0;
             shards[shard]++;
         }
-        var expected = Math.min(Math.pow(2,20), Math.pow(16, level + 1));
-        assert.equal(Object.keys(shards).length, expected, 'shardlevel=' + level + ', shards=' + expected);
+        const expected = Math.min(Math.pow(2,20), Math.pow(16, level + 1));
+        t.equal(Object.keys(shards).length, expected, 'shardlevel=' + level + ', shards=' + expected);
     }
-    assert.end();
+    t.end();
 });
 
