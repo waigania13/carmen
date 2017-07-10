5b1bb2176e8abc83c909f07fca5c3460936fe3bf
Young Hahn <yfhahn@gmail.com>
2017-07-10 05:01:20 -0400

    Fix handling of default text, add tests for konglish case

---
+++ test/geocode-unit.konglish.test.js
@@ -0,0 +1,88 @@
+const tape = require('tape');
+const Carmen = require('..');
+const mem = require('../lib/api-mem');
+const context = require('../lib/context');
+const queue = require('d3-queue').queue;
+const addFeature = require('../lib/util/addfeature'),
+    queueFeature = addFeature.queueFeature,
+    buildQueued = addFeature.buildQueued;
+
+
+(() => {
+    const conf = {
+        place: new mem({ maxzoom: 6, geocoder_languages: ['en','ko'] }, () => {}),
+    };
+    const c = new Carmen(conf);
+
+    tape('index San Francisco CA', (t) => {
+        queueFeature(conf.place, {
+            type: 'Feature',
+            id: 1,
+            properties: {
+                'carmen:center': [1,1],
+                'carmen:text': 'San Francisco',
+                'carmen:text_en': 'San Francisco',
+                'carmen:text_ko': '샌프란시스코',
+                'carmen:score': 10
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [1,1]
+            }
+        }, t.end);
+    });
+    tape('index San Francisco VE', (t) => {
+        queueFeature(conf.place, {
+            type: 'Feature',
+            id: 2,
+            properties: {
+                'carmen:center': [80,-10],
+                'carmen:text': 'San Francisco',
+                'carmen:score': 5
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [80,-10]
+            }
+        }, t.end);
+    });
+    tape('build queued features', (t) => {
+        const q = queue();
+        Object.keys(conf).forEach((c) => {
+            q.defer((cb) => {
+                buildQueued(conf[c], cb);
+            });
+        });
+        q.awaitAll(t.end);
+    });
+
+    tape('query: San Francisco', (t) => {
+        c.geocode('San Francisco', {}, (err, res) => {
+            t.equal('place.1', res.features[0].id, 'Finds SF, CA');
+            t.ifError(err);
+            t.end();
+        });
+    });
+
+    tape('query: San Francisco, language=en', (t) => {
+        c.geocode('San Francisco', { language: 'en' }, (err, res) => {
+            t.equal('place.1', res.features[0].id, 'Finds SF, CA');
+            t.ifError(err);
+            t.end();
+        });
+    });
+
+    tape('query: San Francisco, language=ko', (t) => {
+        c.geocode('San Francisco', { language: 'ko' }, (err, res) => {
+            t.equal('place.1', res.features[0].id, 'Finds SF, CA');
+            t.equal('place.2', res.features[1].id, 'Finds SF, VE');
+            t.ifError(err);
+            t.end();
+        });
+    });
+
+    tape('teardown', (t) => {
+        context.getTile.cache.reset();
+        t.end();
+    });
+})();
