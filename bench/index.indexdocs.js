dc51f6c57e1e57e4773c84048d09b077df0649ef
Young Hahn <yfhahn@gmail.com>
2017-09-23 17:32:13 +0000

    Fix index.indexdocs and add custom docs mode

--- bench/index.indexdocs.js
+++ bench/index.indexdocs.js
@@ -7,8 +7,7 @@ const pointDoc = require('./fixtures/point-doc.json');
 const Carmen = require('..');
 const context = require('../lib/context');
 const mem = require('../lib/api-mem');
-const source = new mem({maxzoom: 6, geocoder_address:1 }, () => {});
-const geocoder = new Carmen({ source: source });
+const geocoder = new Carmen({ source: new mem({maxzoom: 6, geocoder_address:1 }, () => {}) });
 const options = { zoom: 6 };
 const pointDocs = [];
 
@@ -21,47 +20,8 @@ module.exports = benchmark;
 function benchmark(cb) {
   if (!cb) cb = function(){};
   console.log('# indexdocs');
-  suite.add('index many', {
-    'defer': true,
-    'fn': function(deferred) {
-      indexdocs(docs, source, options, function(err, res){
-        deferred.resolve();
-      });
-    }
-  })
-  .add('index large polygon', {
-    'defer': true,
-    'fn': function(deferred) {
-      indexdocs([docs[4]], source, options, function(err, res){
-        deferred.resolve();
-      });
-    }
-  })
-  .add('index small polygon', {
-    'defer': true,
-    'fn': function(deferred) {
-      indexdocs([docs[16]], source, options, function(err, res){
-        deferred.resolve();
-      });
-    }
-  })
-  .add('index point doc', {
-    'defer': true,
-    'fn': function(deferred) {
-      indexdocs([pointDoc], source, options, function(err, res){
-        deferred.resolve();
-      });
-    }
-  })
-  .add('index many point docs', {
-    'defer': true,
-    'fn': function(deferred) {
-      indexdocs(pointDocs, source, options, function(err, res){
-        deferred.resolve();
-      });
-    }
-  })
-  .on('complete', function(event) {
+
+  suite.on('complete', function(event) {
     suite.forEach(function(e) {
       console.log(e.name
         + ' x '
@@ -71,8 +31,74 @@ function benchmark(cb) {
       });
       console.log();
       cb(null, suite);
-  })
-  .run({'async': true});
+  });
+
+  // if a custom geojson file is passed, use it instead
+  if (process.argv[2]) {
+    const fs = require('fs');
+    const filepath = process.argv[2];
+    const customDocs = fs.readFileSync(filepath, 'utf8').split('\n').filter(Boolean).map(JSON.parse);
+    const geocoder = new Carmen({ source: new mem({maxzoom: 6, geocoder_address:1 }, () => {}) });
+    const options = { zoom: 14 };
+    suite.add(`index ${filepath}`, {
+      'defer': true,
+      'fn': function(deferred) {
+        indexdocs(customDocs, geocoder.indexes.source, options, function(err, res) {
+          if (err) throw err;
+          deferred.resolve();
+        });
+      }
+    });
+    suite.run({'async': true});
+    return;
+  }
+
+  suite.add('index many', {
+    'defer': true,
+    'fn': function(deferred) {
+      indexdocs(docs, geocoder.indexes.source, options, function(err, res){
+        if (err) throw err;
+        deferred.resolve();
+      });
+    }
+  });
+  suite.add('index large polygon', {
+    'defer': true,
+    'fn': function(deferred) {
+      indexdocs([docs[4]], geocoder.indexes.source, options, function(err, res){
+        if (err) throw err;
+        deferred.resolve();
+      });
+    }
+  });
+  suite.add('index small polygon', {
+    'defer': true,
+    'fn': function(deferred) {
+      indexdocs([docs[16]], geocoder.indexes.source, options, function(err, res){
+        if (err) throw err;
+        deferred.resolve();
+      });
+    }
+  });
+  suite.add('index point doc', {
+    'defer': true,
+    'fn': function(deferred) {
+      indexdocs([pointDoc], geocoder.indexes.source, options, function(err, res){
+        if (err) throw err;
+        deferred.resolve();
+      });
+    }
+  });
+  suite.add('index many point docs', {
+    'defer': true,
+    'fn': function(deferred) {
+      indexdocs(pointDocs, geocoder.indexes.source, options, function(err, res){
+        if (err) throw err;
+        deferred.resolve();
+      });
+    }
+  });
+  suite.run({'async': true});
 }
 
 if (!process.env.runSuite) benchmark();
