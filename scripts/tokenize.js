737ebba54b719b454e19b92f63a5201b6bef0698
ingalls <ingalls@protonmail.com>
2018-01-19 09:46:35 -0500

    Update tokenize script to better display tokens

--- scripts/tokenize.js
+++ scripts/tokenize.js
@@ -2,20 +2,15 @@
 
 // Apply a termops action on stdin stream and print to stdout.
 
-var fs = require('fs');
-var split = require('split');
-var termops = require('../lib/util/termops.js');
-var type = process.argv[2];
+const readline = require('readline');
+const termops = require('../lib/util/termops.js');
+const type = process.argv[2];
 
-if (type !== 'phrase' && type !== 'term') {
-    console.log('Usage: tokenize.js <phrase|term>');
-    process.exit(1);
-}
+console.error('Enter a token or phrase below to tokenize');
 
-var separator = type === 'phrase' ? ' ' : '\n';
-
-process.stdin
-    .pipe(split())
-    .on('data', function(line) {
-        process.stdout.write(termops.tokenize(line).join(separator) + '\n');
-    });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line', line => {
+    rl.output.write(JSON.stringify(termops.tokenize(line)) + '\n');
+});
