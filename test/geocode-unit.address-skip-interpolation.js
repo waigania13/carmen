f62d3cb148c759304f071b9c89f8b734bb55f002
aarthykc <aarthy@mapbox.com>
2018-01-25 18:19:43 -0500

    add placeholder tests

---
+++ test/geocode-unit.address-skip-interpolation.js
@@ -0,0 +1,61 @@
+const tape = require('tape');
+const Carmen = require('..');
+const mem = require('../lib/api-mem');
+const addFeature = require('../lib/util/addfeature'),
+    queueFeature = addFeature.queueFeature,
+    buildQueued = addFeature.buildQueued;
+
+//skip interpolation for address clusters with large address ranges
+(() => {
+    const conf = {
+        address: new mem({maxzoom: 6, geocoder_address: 1}, () => {})
+    };
+    const c = new Carmen(conf);
+    tape('index address', (t) => {
+        let address = {
+            id:1,
+            properties: {
+                'carmen:text': 'fake street',
+                'carmen:center': [0,0],
+                'carmen:addressnumber': ['100', '101', '102', '104', '106', '5000']
+            },
+            geometry: {
+                type: 'MultiPoint',
+                //this is wrong will change
+                coordinates: [[0,0],[0,0],[0,0],[0,0],[0,0]]
+            }
+        };
+        queueFeature(conf.address, address, () => { buildQueued(conf.address, t.end) });
+    });
+    tape('test address index', (t) => {
+        //check if the address is not interpolised
+        });
+    });
+})();
+
+//address range that increases in progression
+(() => {
+    const conf = {
+        address: new mem({maxzoom: 14, geocoder_address: 1}, function() {})
+    };
+    const c = new Carmen(conf);
+    tape('index address', (t) => {
+        let address = {
+            id:1,
+            properties: {
+                'carmen:text': 'WASHINGTON STREET',
+                'carmen:center': [0,0],
+                'carmen:addressnumber': ['70', '100', '130', '160']
+            },
+            geometry: {
+                type: 'MultiPoint',
+                //change
+                coordinates: [[0,0],[0,0],[0,0], [0,0]]
+            }
+        };
+        queueFeature(conf.address, address, () => { buildQueued(conf.address, t.end) });
+    }); 
+    tape('test address index with a believable range ', (t) => {
+        //should interpolize
+    });
+})();
