842ddf4ef4638b4d8c7346f7c6ccc3feb6050f87
ingalls <ingalls@protonmail.com>
2018-03-19 12:54:58 -0400

    Fix ESLint complaints

--- test/termops.maskAddress.test.js
+++ test/termops.maskAddress.test.js
@@ -8,13 +8,13 @@ test('maskAddress', (q) => {
     q.deepEqual(termops.maskAddress(['1', 'fake', 'street', '100b'], 'fake street ###', parseInt('1110',2)), { addr: '100b', pos: 3 });
     q.deepEqual(termops.maskAddress(['100b', '1', 'fake', 'street'], '', parseInt('1111',2)), { addr: '100b', pos: 0 });
 
-    //Kane IL Style Addresses
+    // Kane IL Style Addresses
     q.deepEqual(termops.maskAddress(['1s13', 'fake', 'street'], '## fake street', parseInt('111',2)), { addr: '1s13', pos: 0 });
     q.deepEqual(termops.maskAddress(['6n486', 'fake', 'street'], '### fake street', parseInt('111',2)), { addr: '6n486', pos: 0 });
     q.deepEqual(termops.maskAddress(['54w32', 'fake', 'street'], '## fake street', parseInt('111',2)), { addr: '54w32', pos: 0 });
     q.deepEqual(termops.maskAddress(['8e234', 'fake', 'street'], '### fake street', parseInt('111',2)), { addr: '8e234', pos: 0 });
 
-    //Waukesha, WI Style Addresses
+    // Waukesha, WI Style Addresses
     q.deepEqual(termops.maskAddress(['w350n5337', 'road', 'c'], '## road c', parseInt('111',2)), { addr: 'w350n5337', pos: 0 });
     q.deepEqual(termops.maskAddress(['n60w35415', 'bayshore', 'circle'], '## bayshore circle', parseInt('111',2)), { addr: 'n60w35415', pos: 0 });
     q.deepEqual(termops.maskAddress(['w35295', 'coastal', 'avenue'], '## coastal avenue', parseInt('111',2)), { addr: 'w35295', pos: 0 });
