618248b294fc603c259e5296b1b64440f278ef7e
nmargolis <nmargolis89@gmail.com>
2018-04-13 13:18:11 -0700

    Add tests for reverse geocoding

--- test/acceptance/geocode-unit.address-routable-point.test.js
+++ test/acceptance/geocode-unit.address-routable-point.test.js
@@ -207,3 +207,67 @@ const { queueFeature, buildQueued } = require('../../lib/indexer/addfeature');
         });
     });
 })();
+
+
+// Test reverse geocoding
+(() => {
+    const conf = {
+        address: new mem({ maxzoom: 6,  geocoder_address:1, geocoder_format: '{address._number} {address._name} {place._name}, {region._name} {postcode._name}, {country._name}' }, () => {}),
+    };
+    const c = new Carmen(conf);
+    tape('index address', (t) => {
+        const address = {
+            id:1,
+            properties: {
+                'carmen:text': 'fake street',
+                'carmen:center': [0,0], // not used
+                'carmen:addressnumber': [null, ['9','11','13']],
+                'carmen:types': ['address']
+            },
+            geometry: {
+                type: 'GeometryCollection',
+                geometries: [
+                    {
+                        type: 'MultiLineString',
+                        coordinates: [
+                            [
+                                [1.111, 1.11],
+                                [1.112, 1.11],
+                                [1.114, 1.11],
+                                [1.115, 1.11]
+                            ]
+                        ]
+                    },
+                    {
+                        type: 'MultiPoint',
+                        coordinates: [[1.111, 1.111], [1.113, 1.111], [1.115, 1.111]]
+                    }
+                ]
+            }
+        };
+        queueFeature(conf.address, address, () => { buildQueued(conf.address, t.end); });
+    });
+
+    tape('Reverse geocode with routingMode', (t) => {
+        c.geocode('1.111, 1.111', { routingMode: true }, (err, res) => {
+            t.ifError(err);
+            t.deepEquals(res.features[0].routable_points,
+                {
+                    points: null
+                },
+                'Reverse geocode with routingMode sets routable_points.points to false'
+            );
+            t.end();
+        });
+    });
+    tape('Reverse geocode without routingMode', (t) => {
+        c.geocode('1.111, 1.111', {}, (err, res) => {
+            t.ifError(err);
+            t.deepEquals(res.features[0].routable_points,
+                undefined,
+                'Reverse geocode without routingMode does not set routable_points'
+            );
+            t.end();
+        });
+    })
+})();
