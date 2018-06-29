5f42e4e666eacf0eb2cb7228ff917287ce702165
aarthykc <aarthy@mapbox.com>
2018-06-29 09:54:02 +0530

    fix tests

--- test/acceptance/geocode-unit.multiload.test.js
+++ test/acceptance/geocode-unit.multiload.test.js
@@ -6,8 +6,10 @@ const mem = require('../../lib/sources/api-mem');
 const queue = require('d3-queue').queue;
 const { queueFeature, buildQueued } = require('../../lib/indexer/addfeature');
 
-const country = new mem(null, () => {});
-const conf = { country: country };
+const conf = {
+    country: new mem({ maxzoom: 6 }, () => {})
+};
+
 const a = new Carmen(conf);
 
 tape('index country', (t) => {
@@ -38,11 +40,11 @@ tape('geocodes', (t) => {
     });
 });
 tape('sets cache/dictcache', (t) => {
-    t.ok(country._geocoder, 'sets source._geocoder on original instance');
-    t.ok(country._dictcache, 'sets source._dictcache on original instance');
-    t.equal(country._geocoder, a.indexes.country._geocoder, 'clone cache === source cache');
-    t.equal(country._dictcache, a.indexes.country._dictcache, 'clone dictcache === source dictcache');
-    const b = new Carmen({ country: country });
+    t.ok(conf.country._geocoder, 'sets source._geocoder on original instance');
+    t.ok(conf.country._dictcache, 'sets source._dictcache on original instance');
+    t.equal(conf.country._geocoder, a.indexes.country._geocoder, 'clone cache === source cache');
+    t.equal(conf.country._dictcache, a.indexes.country._dictcache, 'clone dictcache === source dictcache');
+    const b = new Carmen({ country: conf.country });
     t.equal(b.indexes.country._geocoder, a.indexes.country._geocoder, 'a cache === b cache');
     t.equal(b.indexes.country._dictcache, a.indexes.country._dictcache, 'a dictcache === b dictcache');
     t.end();
