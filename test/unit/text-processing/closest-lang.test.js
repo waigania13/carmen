24db888d4d5d5972e5ba9176894446a0e4aa73a8
samely <eliane@mapbox.com>
2018-08-14 12:21:19 -0400

    Fix linting

--- test/unit/text-processing/closest-lang.test.js
+++ test/unit/text-processing/closest-lang.test.js
@@ -115,7 +115,7 @@ tape('handle nonmatching text with hypens', (t) => {
 
     const bad = 'عربى - السعودية';
 
-    t.equal(closestLangLabel(bad, {'en': 'English'}), undefined, 'non-matching strings that happen to have hyphens should return undefined');
+    t.equal(closestLangLabel(bad, { 'en': 'English' }), undefined, 'non-matching strings that happen to have hyphens should return undefined');
 
     t.end();
 });
