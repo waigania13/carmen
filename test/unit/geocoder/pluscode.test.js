14707af36b449766d777f3da399073d54b37f23f
ingalls <ingalls@protonmail.com>
2018-08-24 10:17:53 -0400

    Add isValid, isShort, isFull tests

--- test/unit/geocoder/pluscode.test.js
+++ test/unit/geocoder/pluscode.test.js
@@ -1,7 +1,43 @@
 'use strict';
-const address = require('../../../lib/geocoder/pluscode');
+const pluscode = require('../../../lib/geocoder/pluscode');
 const test = require('tape');
 
-test('pluscode ', (t) => {
+test('pluscode validity tests', (t) => {
+
+    //Test data for validity tests.
+    //Format of each array is: [code,isValid,isShort,isFull]
+
+    const fixtures = [
+        // Valid full codes:
+        ['8FWC2345+G6',true,false,true],
+        ['8FWC2345+G6G',true,false,true],
+        ['8fwc2345+',true,false,true],
+        ['8FWCX400+',true,false,true],
+
+        // Valid short codes:
+        ['WC2345+G6g',true,true,false],
+        ['2345+G6',true,true,false],
+        ['45+G6',true,true,false],
+        ['+G6',true,true,false],
+
+        // Invalid codes
+        ['G+',false,false,false],
+        ['+',false,false,false],
+        ['8FWC2345+G',false,false,false],
+        ['8FWC2_45+G6',false,false,false],
+        ['8FWC2η45+G6',false,false,false],
+        ['8FWC2345+G6+',false,false,false],
+        ['8FWC2300+G6',false,false,false],
+        ['WC2300+G6g',false,false,false],
+        ['WC2345+G',false,false,false]
+    ];
+
+    for (const fixture of fixtures) {
+        t.equals(pluscode.isValid(fixture[0]), fixture[1], `isValid('${fixture[0]}') should be ${fixture[1] ? 'true' : 'false'}`);
+        t.equals(pluscode.isShort(fixture[0]), fixture[2], `isShort('${fixture[0]}') should be ${fixture[2] ? 'true' : 'false'}`);
+        t.equals(pluscode.isFull(fixture[0]), fixture[3], `isFull('${fixture[0]}') should be ${fixture[3] ? 'true' : 'false'}`);
+    }
+
+
     t.end();
 });
