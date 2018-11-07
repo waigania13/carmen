475e60d88b3f2993e3dc55c0c9c0365db52355f6
Jeff Miccolis <jeff@miccolis.net>
2018-11-07 15:44:30 -0500

    Eliminate address + distance deduplication

--- test/unit/geocoder/dedupe.test.js
+++ test/unit/geocoder/dedupe.test.js
@@ -2,35 +2,35 @@
 const dedupe = require('../../../lib/geocoder/dedupe');
 const tape = require('tape');
 
-tape('dedup lowercase vs caps', (t) => {
-    const features = [
-        {
-            place_name: '20 main st',
-            text: 'main st',
-            address: 20,
-            center:[0,0],
-            geometry: {
-                type:'Point',
-                coordinates:[0,0]
-            }
-        },
-        {
-            place_name: '20 MAIN ST',
-            text: 'MAIN ST',
-            address: 20,
-            center:[0,0],
-            geometry: {
-                type:'Point',
-                coordinates:[0,0]
-            }
-        }
-    ];
-    t.deepEqual(dedupe(features), [
-        features[0]
-    ], 'dedupes by lowercase vs caps');
-
-    t.end();
-});
+// tape('dedup lowercase vs caps', (t) => {
+//     const features = [
+//         {
+//             place_name: '20 main st',
+//             text: 'main st',
+//             address: 20,
+//             center:[0,0],
+//             geometry: {
+//                 type:'Point',
+//                 coordinates:[0,0]
+//             }
+//         },
+//         {
+//             place_name: '20 MAIN ST',
+//             text: 'MAIN ST',
+//             address: 20,
+//             center:[0,0],
+//             geometry: {
+//                 type:'Point',
+//                 coordinates:[0,0]
+//             }
+//         }
+//     ];
+//     t.deepEqual(dedupe(features), [
+//         features[0]
+//     ], 'dedupes by lowercase vs caps');
+//
+//     t.end();
+// });
 
 tape('dedupe', (t) => {
     let features = [
@@ -67,115 +67,23 @@ tape('dedupe', (t) => {
         features[1]
     ], 'dedupes by place_name');
 
-    features = [
-        {
-            place_name: '100 main st springfield 00001',
-            address:100,
-            text: 'main st',
-            center:[0,0],
-            geometry: {
-                type:'Point',
-                coordinates:[0,0]
-            }
-        },
-        {
-            place_name: '100 main st springfield 00002',
-            address:100,
-            text: 'main st',
-            center:[20,0],
-            geometry: {
-                type:'Point',
-                coordinates:[20,0]
-            }
-        },
-    ];
-    t.deepEqual(dedupe(features), [
-        features[0],
-        features[1],
-    ], 'dupe identical addresses when dist >= 5km');
 
     features = [
         {
-            place_name: '100 main st springfield 00001',
-            address:100,
-            text: 'main st',
-            center:[0.000,0],
-            geometry: {
-                type:'Point',
-                coordinates:[0.000,0]
-            }
-        },
-        {
-            place_name: '100 main st springfield 00002',
-            address:100,
-            text: 'main st',
-            center:[0.001,0],
-            geometry: {
-                type:'Point',
-                coordinates:[0.001,0]
-            }
-        },
-    ];
-    t.deepEqual(dedupe(features), [
-        features[0]
-    ], 'dedupes identical addresses when dist < 5km');
-
-    features = [
-        {
-            place_name: '100 main st springfield 00001',
-            address:100,
-            text: 'main st',
-            center:[0.000,0],
-            geometry: {
-                type:'Point',
-                coordinates:[0.000,0]
-            }
-        },
-        {
-            place_name: '100 main st springfield 00002',
-            address:100,
-            text: 'main st',
-            center:[0.001,0],
-            geometry: {
-                type:'Point',
-                coordinates:[0.001,0]
-            }
-        },
-        {
-            place_name: '100 main st springfield 00001',
-            address:100,
-            text: 'main st',
-            center:[1,0],
-            geometry: {
-                type:'Point',
-                coordinates:[1,0]
-            }
-        }
-    ];
-    t.deepEqual(dedupe(features), [
-        features[0]
-    ], 'dedupes identical addresses + placenames when dist < 5km');
-
-    features = [
-        {
-            place_name: '100 main st springfield 00001',
-            address:100,
+            place_name: 'main st springfield 00001',
             text: 'main st',
             center:[0.000,0],
             geometry: {
                 omitted: true,
-                interpolated: true,
                 type:'Point',
                 coordinates:[0.000,0]
             }
         },
         {
-            place_name: '100 main st springfield 00002',
-            address:100,
+            place_name: 'main st springfield 00001',
             text: 'main st',
             center:[0.001,0],
             geometry: {
-                interpolated: true,
                 type:'Point',
                 coordinates:[0.001,0]
             }
@@ -183,13 +91,13 @@ tape('dedupe', (t) => {
     ];
     t.deepEqual(dedupe(features), [
         features[1]
-    ], 'dedupes identical addresses, prioritizes non-omitted');
+    ], 'dedupes, prioritizes non-omitted');
 
     // Reverse to make sure logic works in reverse order.
     features.reverse();
     t.deepEqual(dedupe(features), [
         features[0]
-    ], 'dedupes identical addresses, prioritizes non-omitted');
+    ], 'dedupes, prioritizes non-omitted');
 
     t.end();
 });
