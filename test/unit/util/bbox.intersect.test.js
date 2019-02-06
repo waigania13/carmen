4cc764a7cbb30c9ec3c10d5e1b24a3948d0d932e
Andrew Pendleton <andrew@mapbox.com>
2019-02-06 01:36:44 -0500

    improve test coverage

--- test/unit/util/bbox.intersect.test.js
+++ test/unit/util/bbox.intersect.test.js
@@ -7,6 +7,7 @@ tape('check if polygons intersect', (t) => {
     const bb2 = [-66, 34, -64, 44];
     t.assert(BBox.intersect(bb1, bb2));
     t.assert(BBox.amIntersect(bb1, bb2));
+    t.deepEqual(BBox.intersection(bb1, bb2), [-66, 35, -65, 44]);
     t.end();
 });
 
@@ -15,6 +16,7 @@ tape('check if polygons do not intersect', (t) => {
     const bb2 = [-66, -44, -64, -34];
     t.assert(!BBox.intersect(bb1, bb2));
     t.assert(!BBox.amIntersect(bb1, bb2));
+    t.deepEqual(BBox.intersection(bb1, bb2), false);
     t.end();
 });
 
